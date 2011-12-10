5e2bb97229551ae2648641e77418815ff2ec6238
Yan Zhou <zhouyan@me.com>
2011-12-10 13:40:41 +0000

    refectory history and particle

--- include/vSMC/history.hpp
+++ include/vSMC/history.hpp
@@ -9,75 +9,49 @@ namespace vSMC {
 
 enum HistoryMode {HISTORY_RAM, HISTORY_FILE, HISTORY_NONE};
 
-template <class PartType>
+template <class T>
 class HistoryElement
 {
     public :
 
-    HistoryElement (Particle<PartType> &particle,
+    HistoryElement (Particle<T> &particle,
             bool resample, double ess, std::size_t accept) :
         e_particle(particle), e_resample(resample),
         e_ess(ess), e_accept(accept) {}
 
-    HistoryElement (const HistoryElement<PartType> &element) :
-        e_particle(element.particle), e_resample(element.resample),
-        e_ess(element.ess), e_accept(element.accept) {}
-
-    HistoryElement<PartType> & operator= (
-            const HistoryElement<PartType> &element)
-    {
-        if (&element != this) {
-            e_particle = element.particle;
-            e_resample = element.resample;
-            e_ess = element.ess;
-            e_accept = element.accept;
-        }
-
-        return *this;
-    }
-
     private :
 
-    Particle<PartType> e_particle;
+    Particle<T> e_particle;
     bool e_resample;
     double e_ess;
     std::size_t e_accept;
 }; // class HistoryElement
 
-template <class PartType>
+template <class T>
 class History
 {
     public :
 
-    inline void push_back (const HistoryElement<PartType> &element);
+    inline void push_back (const HistoryElement<T> &element)
+    {
+        history.push_back(element);
+    }
 
-    inline void pop_back () const;
+    inline void pop_back ()
+    {
+        history.pop_back();
+    }
 
-    inline void pop_back (HistoryElement<PartType> &element) const;
+    inline void pop_back (HistoryElement<T> &element)
+    {
+        element = history.back();
+        history.pop_back();
+    }
 
     private :
 
-    std::vector<HistoryElement<PartType> > history;
+    std::vector<HistoryElement<T> > history;
 }; // class History
-
-template <class PartType>
-void History<PartType>::push_back (const HistoryElement<PartType> &element)
-{
-    history.push_back(element);
-}
-
-template <class PartType>
-void History<PartType>::pop_back ()
-{
-    history.pop_back();
-}
-
-template <class PartType>
-void History<PartType>::pop_back (HistoryElement<PartType> &element)
-{
-    element = history.back();
-    pop_back();
-}
 
 } // namespace vSMC
 
--- include/vSMC/particle.hpp
+++ include/vSMC/particle.hpp
@@ -4,8 +4,6 @@
 #include <limits>
 #include <cmath>
 #include <cstddef>
-#include <cstdlib>
-#include <ctime>
 #include <mkl_vml.h>
 #include <mkl_vsl.h>
 #include <gsl/gsl_rng.hp>
@@ -14,52 +12,82 @@
 
 namespace vSMC {
 
+/// The resampling scheme
 enum ResampleScheme {MULTINOMIAL, RESIDUAL, STRATIFIED, SYSTEMATIC};
 
-template <class PartType>
+/// \brief Particle contains a set of particles and their weights.
+///
+/// The template parameter \b T is usually a container of the actual particel
+/// types. It can also be other types other than a container. The important
+/// thing is that it is not a single particle, but a set of them at a certain
+/// iteration of the SMC algorithm. It has to have a contructor which accept
+/// an integral type parameter, which is the number of particles, and the user
+/// need to provide a function that tell Particle how to copy one particle to
+/// another's position. A STL sequential container satifies these requirements
+template <class T>
 class Particle
 {
     public :
 
-    Particle (std::size_t N,
-            void (*copy)(std::size_t, std::size_t, PartType &)) :
+    /// \brief The constructor of Particle. There is no default constructor
+    ///
+    /// \param N The number of particles
+    /// \param copy The pointer to a function which tell Particle how to copy
+    /// a particle at position \b n1 to position \b n2.
+    Particle (std::size_t N, void (*copy)(std::size_t, std::size_t, T &)) :
         particle_num(N), particle(N), weight(N), log_weight(N),
-        weight_r1m(0), weight_r2m(0), replication(N), copy_particle(copy)
+        replication(N), copy_particle(copy) {}
+
+    /// \brief Set the log(weights)
+    ///
+    /// \param [in] new_weight The pointer to the new log weights to be set
+    void SetLogWeight (const double *new_weight)
     {
-        vsldSSNewTask(&ess_task, 1, N, VSL_SS_MATRIX_STORAGE_COLS,
-                weight, NULL, NULL);
-        vsldSSEditMoments(ess_task, &weight_r1m, &weight_r2m,
-                NULL, NULL, NULL, NULL, NULL);
-        std::time_t curr_time = std::time(NULL);
-        std::size_t max_int = std::numeric_limits<int>::max();
-        std::size_t rng_seed = curr_time % max_int + 1;
-        rng_gsl = gsl_rng_alloc(gsl_rng_mt19937);
-        gsl_rng_set(rng_gsl, rng_seed);
+        cblas_dcopy(particle_num, new_weight, 1, log_weight, 1)
     }
 
-    Particle (const Particle<PartType> &part)
-        particle_num (part.particle_num),
-        particle     (part.particle),
-        weight       (part.weight),
-        log_weight   (part.log_weight),
-        weight_r1m   (part.weight_r1m),
-        weight_r2m   (part.weight_r2m),
-        replication  (part.replication),
-        copy_particle(part.copy_particle)
-        {
+    /// \brief Add to the log(weights)
+    ///
+    /// \param [in] inc_weight The pointer to the incremental log weights to
+    /// be added.
+    void AddLogWeight (const double *inc_weight);
+    {
+        vdAdd(particle_num, log_weight, inc_weight, log_weight)
+    }
+
+    /// \brief Return the ESS
+    ///
+    /// \return The ESS calculated with current weights
+    double ESS ()
+    {
+        require_weight();
+        vdSqr(particle_num, weight, weight);
+        return 1 / cblas_dasum(particle_num, weight, 1);
+    }
+
+    /// \brief Perform the resampling
+    ///
+    /// \param scheme See ResampleScheme
+    /// \param rng The pointer to a GSL RNG object
+    void Resample (ResampleScheme scheme, const gsl_rng *rng)
+    {
+        switch (scheme) {
+            case MULTINOMIAL :
+                resample_multinomial(rng);
+                break;
+            case RESIDUAL :
+                resample_residual(rng);
+                break;
+            case STRATIFIED :
+                resample_stratified(rng);
+                break;
+            case SYSTEMATIC :
+                resample_systematic(rng);
+                break;
+            default :
+                resample_residual(rng);
         }
-
-    ~Particle ()
-    {
-        vslSSDeleteTask(ess_task);
-        gsl_rng_free(rng_gsl);
     }
-
-    inline void SetLogWeight (const double *new_weight);
-    inline void AddLogWeight (const double *inc_weight);
-
-    inline double ESS ();
-    inline void Resample (ResampleScheme scheme);
 
     private :
 
@@ -67,132 +95,66 @@ class Particle
     typedef vDist::internal::Buffer<std::size_t> uBuffer;
 
     std::size_t particle_num;
-    PartType particle;
+    T particle;
     dBuffer weight;
     dBuffer log_weight;
     uBuffer replication;
-    double weight_r1m;
-    double weight_r2m;
+    void (*copy_particle) (std::size_t n1, std::size_t n2, T &particles);
 
-    VSLSSTaskPtr ess_task;
-    gsl_rng *rng_gsl;
-
-    void (*copy_particle) (
-            std::size_t n1, std::size_t n2, PartType &particles);
-
-    inline void require_weight ();
-
-    inline void resample_multinomial ();
-    inline void resample_residual ();
-    inline void resample_stratified ();
-    inline void resample_systematic ();
-    inline void resample_do ();
-}; // class Particle
-
-template <class PartType>
-double Particle<PartType>::SetLogWeight (const double *new_weight)
-{
-    cblas_dcopy(particle_num, new_weight, 1, log_weight, 1);
-}
-
-template <class PartType>
-double Particle<PartType>::AddLogWeight (const double *inc_weight)
-{
-    vdAdd(particle_num, log_weight, inc_weight, log_weight);
-}
-
-template <class PartType>
-void Particle<PartType>::require_weight ()
-{
-    vdExp(particle_num, log_weight, weight);
-}
-
-template <class PartType>
-double Particle<PartType>::ESS ()
-{
-    require_weight();
-    vsldSSCompute(ess_task, VSL_SS_MEAN|VSL_SS_2R_MOM, VSL_SS_METHOD_TBS);
-
-    return particle_num * weight_r1m * weight_r1m / weight_r2m;
-}
-
-
-template <class PartType>
-void Particle<PartType>::Resample (ResampleScheme scheme)
-{
-    switch (scheme) {
-        case MULTINOMIAL :
-            resample_multinomial();
-            break;
-        case RESIDUAL :
-            resample_residual();
-            break;
-        case STRATIFIED :
-            resample_stratified();
-            break;
-        case SYSTEMATIC :
-            resample_systematic();
-            break;
-        default :
-            resample_residual();
-    }
-}
-
-template <class PartType>
-void Particle<PartType>::resample_multinomial ()
-{
-    require_weight();
-    gsl_ran_multinomial(rng_gsl, particle_num, particle_num,
-            weight, replication);
-    resample_do();
-}
-
-template <class PartType>
-void Particle<PartType>::resample_residual ()
-{
-    require_weight();
-    cblas_dscal(particle_num, particle_num, weight, 1);
-    vdFloor(particle_num, weight, weight);
-    std::size_t size = particle_num - cblas_dasum(particle_num, weight, 1);
-    gsl_ran_multinomial(rng_gsl, particle_num, size, weight, replication);
-    for (std::size_t i = 0; i != particle_num, ++i)
-        replication[i] += weight[i];
-    resample_do();
-}
-
-template <class PartType>
-void Particle<PartType>::resample_stratified ()
-{
-    resample_do();
-}
-
-template <class PartType>
-void Particle<PartType>::resample_systematic ()
-{
-    resample_do();
-}
-
-template <class PartType>
-void Particle<PartType>::resample_do ()
-{
-    std::size_t from = 0;
-    std::size_t time = 0;
-
-    for (std::size_t i = 0; i != particle_num; ++i)
+    void require_weight ()
     {
-        if (!replication[i]) { // replication[i] has zero child
-            if (time == replication[from]) {
-                // all childs of replication[from] are already copied
-                time = 0;
-                ++from;
-                while (!replication[from])
+        vdExp(particle_num, log_weight, weight);
+    }
+
+    void resample_multinomial (const gsl_rng *rng)
+    {
+        require_weight();
+        gsl_ran_multinomial(rng, particle_num, particle_num,
+                weight, replication);
+
+        resample_do();
+    }
+
+    void resample_residual (const gsl_rng *rng);
+    {
+        require_weight();
+        cblas_dscal(particle_num, particle_num, weight, 1);
+        vdFloor(particle_num, weight, weight);
+
+        std::size_t size = particle_num;
+        size -= cblas_dasum(particle_num, weight, 1);
+        gsl_ran_multinomial(rng, particle_num, size, weight, replication);
+
+        for (std::size_t i = 0; i != particle_num, ++i)
+            replication[i] += weight[i];
+
+        resample_do();
+    }
+
+    void resample_stratified (const gsl_rng *rng) {}
+    void resample_systematic (const gsl_rng *rng) {}
+
+    void resample_do ()
+    {
+        std::size_t from = 0;
+        std::size_t time = 0;
+
+        for (std::size_t i = 0; i != particle_num; ++i)
+        {
+            if (!replication[i]) { // replication[i] has zero child
+                if (time == replication[from]) {
+                    // all childs of replication[from] are already copied
+                    time = 0;
                     ++from;
+                    while (!replication[from])
+                        ++from;
+                }
+                (*copy_particle)(from, i, partile_set);
+                ++time;
             }
-            (*copy_particle)(from, i, partile_set);
-            ++time;
         }
     }
-}
+}; // class Particle
 
 } // namespace vSMC
 
