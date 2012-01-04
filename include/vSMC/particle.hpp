d58118d288f0821ac1b0db11da60b824f8ce2f01
Yan Zhou <zhouyan@me.com>
2012-01-04 10:55:32 +0000

    remote value_type in Particle<T>

--- include/vSMC/particle.hpp
+++ include/vSMC/particle.hpp
@@ -27,8 +27,6 @@ class Particle
 {
     public :
 
-    typedef T value_type;
-
     Particle (std::size_t N, void (*copy)(std::size_t, std::size_t, T &)) :
         pnum(N), pval(N), weight(N), log_weight(N),
         copy_particle(copy) {}
