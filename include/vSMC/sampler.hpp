22487b48668797a8efe1dfc4406c0ad71c3157ac
Yan Zhou <zhouyan@me.com>
2012-02-21 23:57:10 +0000

    documentation of the examples

--- include/vSMC/sampler.hpp
+++ include/vSMC/sampler.hpp
@@ -18,6 +18,28 @@
 #include <vSMC/particle.hpp>
 #include <vSMC/path.hpp>
 
+/*************************************************************************//**
+ * \page pf_seq Sequential implementation of particle filter example
+ *
+ * This example is implemented with sequential code. Most of the code is no
+ * more complex than those found in SMCTC. This is only a proof of concept that
+ * vSMC can also be used in a similar fashion as in SMCTC. But for sequential
+ * implementation, SMCTC is much more stable and matured. For a vectorized
+ * implementation, which really takes the advantage of vSMC, see \ref pf_eigen.
+ *
+ * \include pf/pf_seq.cpp
+ *
+ * \page pf_eigen Vectorized implementation of particle filter example
+ *
+ * This example is implemented with the Eigen library. Its syntax is quite
+ * elegent and in most case its use shall be self-explained by the code. It
+ * also use the vDist library for vectorized computation of pdf, kernel,
+ * generating random variates, etc. For a sequential implementation without any
+ * use of SIMD see \ref pf_seq.
+ *
+ * \include pf/pf_eigen.cpp
+ ****************************************************************************/
+
 namespace vSMC {
 
 template <typename T>
