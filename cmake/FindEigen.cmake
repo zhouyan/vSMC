8a2e72692fb938cfe65a677dc6205224374b42fe
Yan Zhou <zhouyan@me.com>
2012-02-24 22:19:57 +0000

    move example to test, use cmake for build tests

---
+++ cmake/FindEigen.cmake
@@ -0,0 +1,13 @@
+SET (Eigen_FOUND TRUE)
+
+SET (Eigen_INC_PATH ${Eigen_INC_PATH} /usr/local/include/eigen3)
+SET (Eigen_INC_PATH ${Eigen_INC_PATH} /usr/include/eigen3)
+
+FIND_PATH (Eigen_INCLUDE_DIR signature_of_eigen3_matrix_library
+    PATHS ${Eigen_INC_PATH} ENV INCLUDE)
+IF (${Eigen_INCLUDE_DIR} STREQUAL Eigen_INCLUDE_DIR-NOTFOUND)
+    MESSAGE (STATUS "NOT Found Eigen headers")
+    SET (Eigen_FOUND FALSE)
+ELSE (${Eigen_INCLUDE_DIR} STREQUAL Eigen_INCLUDE_DIR-NOTFOUND)
+    MESSAGE (STATUS "Found Eigen headers: ${Eigen_INCLUDE_DIR}")
+ENDIF (${Eigen_INCLUDE_DIR} STREQUAL Eigen_INCLUDE_DIR-NOTFOUND)
