ebf56a6cb19886862d299fc3d932ba3d7f0f52a7
Yan Zhou <zhouyan@me.com>
2012-04-17 14:12:25 +0100

    implement beta and laplace distributions in boost::random

--- include/vSMC/rng/beta_distribution.hpp
+++ include/vSMC/rng/beta_distribution.hpp
@@ -1,14 +1,25 @@
-#ifndef V_SMC_RNG_BETA_DISTRIBUTION_HPP
-#define V_SMC_RNG_BETA_DISTRIBUTION_HPP
+#ifndef BOOST_RANDOM_BETA_DISTRIBUTION_HPP
+#define BOOST_RANDOM_BETA_DISTRIBUTION_HPP
 
-#include <limits>
-#include <cassert>
-#include <cmath>
-#include <boost/math/special_functions/log1p.hpp>
-#include <boost/random/gamma_distribuiton.hpp>
+#include <istream>
+#include <iosfwd>
+#include <boost/assert.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/random/detail/config.hpp>
+#include <boost/random/detail/operators.hpp>
+#include <boost/random/gamma_distribution.hpp>
 
-namespace vSMC {
+namespace boost { namespace random {
 
+/**
+ * Instantiations of class template beta_distribution model a
+ * \random_distribiton. Such a distribution produces random numbers @c x
+ * distributed with probability density function
+ * \f$\displaystyle p(x) =
+ *   \frac{1}{B(\alpha,\beta)}x^{\alpha-1}(1-x)^{\beta-1}
+ * \f$,
+ * where shape1 and shape2 are the paramters of the distributions.
+ */
 template <typename RealType = double>
 class beta_distribution
 {
@@ -23,68 +34,155 @@ class beta_distribution
 
         typedef beta_distribution distribution_type;
 
-        explicit param_type (RealType shape1 = 0, RealType shape2 = 1) :
-            shape1_(shape1), shape2_(shape2) {}
+        /**
+         * Constructs a @c param_type with a given shape1 and shape2.
+         *
+         * Requires: shape1 > 0 && shape2 > 0
+         */
+        explicit param_type (
+                RealType shape1_arg = RealType(1.0),
+                RealType shape2_arg = RealType(1.0)) :
+            _shape1(shape1_arg), _shape2(shape2_arg) {}
 
-        RealType shape1 () const {return shape1_;}
-        RealType shape2 () const {return shape2_;}
+        /** Returns the shape1 of the distribution. */
+        RealType shape1 () const {return _shape1;}
 
+        /** Returns the shape2 of the distribution. */
+        RealType shape2 () const {return _shape2;}
+
+        /** Writes a @c param_type to a @c std::ostream. */
+        BOOST_RANDOM_DETAIL_OSTREAM_OPERATOR(os, param_type, parm)
+        { os << parm._shape1 << " " << parm._shape2 ; return os; }
+
+        /** Reads a @c param_type from a @c std::istream. */
+        BOOST_RANDOM_DETAIL_ISTREAM_OPERATOR(is, param_type, parm)
+        { is >> parm._shape1 >> std::ws >> parm._shape2; return is; }
+
+        /** Returns true if the two sets of parameters are the same. */
+        BOOST_RANDOM_DETAIL_EQUALITY_OPERATOR(param_type, lhs, rhs)
+        { return lhs._shape1 == rhs._shape1 && lhs._shape2 == rhs._shape2; }
+        
+        /** Returns true if the two sets of parameters are the different. */
+        BOOST_RANDOM_DETAIL_INEQUALITY_OPERATOR(param_type)
         private :
 
-        RealType shape1_;
-        RealType shape2_;
+        RealType _shape1;
+        RealType _shape2;
     }; // class param_type
 
-    explicit beta_distribution (RealType shape1 = 0, RealType  shape2 = 1) :
-        shape1_(shape1), shape2_(shape2)
+    /**
+     * Constructs a @c beta_distribution objet. @c shape1 and @c shape2 are
+     * the parameters for the distribution.
+     *
+     * Requires: shape1 > 0 && shape2 > 0
+     */
+    explicit beta_distribution (
+            const RealType &shape1_arg = RealType(1.0),
+            const RealType &shape2_arg = RealType(1.0)) :
+        _shape1(shape1_arg), _shape2(shape2_arg)
     {
-        assert(shape1_ > 0);
-        assert(shape2_ > 0);
+        BOOST_ASSERT(_shape1 > RealType(0.0));
+        BOOST_ASSERT(_shape2 > RealType(0.0));
     }
 
-    explicit beta_distribution (const param_type &param) :
-        shape1_(param.shape1()), shape2_(param.shape2())
+    /**
+     * Constructs a @c beta_distribution object from its parameters.
+     */
+    explicit beta_distribution (const param_type &parm) :
+        _shape1(parm.shape1()), _shape2(parm.shape2())
     {
-        assert(shape1_ > 0);
-        assert(shape2_ > 0);
+        BOOST_ASSERT(_shape1 > RealType(0.0));
+        BOOST_ASSERT(_shape2 > RealType(0.0));
     }
 
-    RealType min () const {return 0;}
-    RealType max () const {return 1;}
-    RealType shape1 () const {return shape1_;}
-    RealType shape2 () const {return shape2_;}
-    param_type param () const {return param_type(shape1_, shape2_);}
-    void param (const param_type &param)
+    /** Returns the shape1 of the distribution. */
+    RealType shape1 () const {return _shape1;}
+
+    /** Returns the shape2 of the distribution. */
+    RealType shape2 () const {return _shape2;}
+
+    /** Returns the smallest value that the distribution can produce. */
+    RealType min BOOST_PREVENT_MACRO_SUBSTITUTION () const
+    {return RealType(0.0);}
+
+    /** Returns the largest value that the distribution can produce. */
+    RealType max BOOST_PREVENT_MACRO_SUBSTITUTION () const
+    {return RealType(1.0);}
+
+    /** Returns the parameters of the distribution. */
+    param_type param () const {return param_type(_shape1, _shape2);}
+
+    /** Sets the parameters of the distribution. */
+    void param (const param_type &parm)
     {
-        shape1_ = param.shape1();
-        shape2_ = param.shape2();
+        _shape1 = parm.shape1();
+        _shape2 = parm.shape2();
     }
 
+    /**
+     * Effects: Subsequent uses of the distribution do not depend
+     * on values produced by any engine prior to invoking reset.
+     */
     void reset () {}
 
+    /** Returns a beta variate. */
     template <typename Engine>
     result_type operator() (Engine &eng)
     {
         RealType x =
-            boost::random::gamma_distribution<RealType>(shape1_, 1)(eng);
+            boost::random::gamma_distribution<RealType>(_shape1, 1)(eng);
         RealType y =
-            boost::random::gamma_distribution<RealType>(shape2_, 1)(eng);
+            boost::random::gamma_distribution<RealType>(_shape2, 1)(eng);
 
         return x / (x + y);
     }
 
-    template <typename Engine>
-    result_type operator() (Eigen &eng, const param_type &param)
+    /** Returns a beta variate with parameters specified by @c param. */
+    template <typename URNG>
+    result_type operator() (URNG &urng, const param_type &parm)
     {
-        return beta_distribution(param)(eng);
+        return beta_distribution(parm)(urng);
     }
+
+    /** Writes a @c beta_distribution to a @c std::ostream. */
+    BOOST_RANDOM_DETAIL_OSTREAM_OPERATOR(os, beta_distribution, nd)
+    {
+        os << nd._shape1 << " " << nd._shape2 << " ";
+        return os;
+    }
+
+    /** Reads a @c beta_distribution from a @c std::istream. */
+    BOOST_RANDOM_DETAIL_ISTREAM_OPERATOR(is, beta_distribution, nd)
+    {
+        is >> std::ws >> nd._shape1 >> std::ws >> nd._shape2;
+        return is;
+    }
+
+    /**
+     * Returns true if the two instances of @c beta_distribution will
+     * return identical sequences of values given equal generators.
+     */
+    BOOST_RANDOM_DETAIL_EQUALITY_OPERATOR(beta_distribution, lhs, rhs)
+    {
+        return lhs._shape1 == rhs._shape2 && lhs._shape1 == rhs._shape2;
+    }
+
+    /**
+     * Returns true if the two instances of @c beta_distribution will
+     * return different sequences of values given equal generators.
+     */
+    BOOST_RANDOM_DETAIL_INEQUALITY_OPERATOR(beta_distribution)
 
     private :
 
-    RealType shape1_;
-    RealType shape2_;
+    RealType _shape1;
+    RealType _shape2;
 }; // class beta_distribution
 
-} // namespace vSMC
+} // namespace random
 
-#endif // V_SMC_RNG_BETA_DISTRIBUTION_HPP
+using random::beta_distribution;
+
+} // namespace boost
+
+#endif // BOOST_RANDOM_BETA_DISTRIBUTION_HPP
--- include/vSMC/rng/laplace_distribution.hpp
+++ include/vSMC/rng/laplace_distribution.hpp
@@ -1,14 +1,28 @@
-#ifndef V_SMC_RNG_LAPLACE_DISTRIBUTION_HPP
-#define V_SMC_RNG_LAPLACE_DISTRIBUTION_HPP
+#ifndef BOOST_RANDOM_LAPLACE_DISTRIBUTION_HPP
+#define BOOST_RANDOM_LAPLACE_DISTRIBUTION_HPP
 
-#include <limits>
-#include <cassert>
-#include <cmath>
+#include <boost/config/no_tr1/cmath.hpp>
+#include <istream>
+#include <iosfwd>
+#include <boost/assert.hpp>
+#include <boost/limits.hpp>
+#include <boost/static_assert.hpp>
 #include <boost/math/special_functions/log1p.hpp>
+#include <boost/random/detail/config.hpp>
+#include <boost/random/detail/operators.hpp>
 #include <boost/random/uniform_01.hpp>
 
-namespace vSMC {
+namespace boost { namespace random {
 
+/**
+ * Instantiations of class template laplace_distribution model a
+ * \random_distribiton. Such a distribution produces random numbers @c x
+ * distributed with probability density function
+ * \f$\displaystyle p(x) =
+ *   \frac{1}{2b}\exp(-|x-\mu|/b)
+ * \f$,
+ * where location and scale are the paramters of the distributions.
+ */
 template <typename RealType = double>
 class laplace_distribution
 {
@@ -23,68 +37,156 @@ class laplace_distribution
 
         typedef laplace_distribution distribution_type;
 
-        explicit param_type (RealType location = 0, RealType scale = 1) :
-            location_(location), scale_(scale) {}
+        /**
+         * Constructs a @c param_type with a given location and scale.
+         *
+         * Requires: scale >= 0
+         */
+        explicit param_type (
+                RealType location_arg = RealType(0.0),
+                RealType scale_arg = RealType(1.0)) :
+            _location(location_arg), _scale(scale_arg) {}
 
-        RealType location () const {return location_;}
-        RealType scale () const {return scale_;}
+        /** Returns the location of the distribution. */
+        RealType location () const {return _location;}
 
+        /** Returns the scale of the distribution. */
+        RealType scale () const {return _scale;}
+
+        /** Writes a @c param_type to a @c std::ostream. */
+        BOOST_RANDOM_DETAIL_OSTREAM_OPERATOR(os, param_type, parm)
+        { os << parm._location << " " << parm._scale ; return os; }
+
+        /** Reads a @c param_type from a @c std::istream. */
+        BOOST_RANDOM_DETAIL_ISTREAM_OPERATOR(is, param_type, parm)
+        { is >> parm._location >> std::ws >> parm._scale; return is; }
+
+        /** Returns true if the two sets of parameters are the same. */
+        BOOST_RANDOM_DETAIL_EQUALITY_OPERATOR(param_type, lhs, rhs)
+        { return lhs._location == rhs._location && lhs._scale == rhs._scale; }
+        
+        /** Returns true if the two sets of parameters are the different. */
+        BOOST_RANDOM_DETAIL_INEQUALITY_OPERATOR(param_type)
         private :
 
-        RealType location_;
-        RealType scale_;
+        RealType _location;
+        RealType _scale;
     }; // class param_type
 
+    /**
+     * Constructs a @c laplace_distribution objet. @c location and @c scale
+     * are the parameters for the distribution.
+     *
+     * Requires: scale > 0
+     */
     explicit laplace_distribution (
-            RealType location = 0, RealType  scale = 1) :
-        location_(location), scale_(scale)
+            const RealType &location_arg = RealType(1.0),
+            const RealType &scale_arg = RealType(1.0)) :
+        _location(location_arg), _scale(scale_arg)
     {
-        assert(scale_ >= 0);
+        BOOST_ASSERT(_scale >= RealType(0.0));
     }
 
-    explicit laplace_distribution (const param_type &param) :
-        location_(param.location()), scale_(param.scale())
+    /**
+     * Constructs a @c laplace_distribution object from its parameters.
+     */
+    explicit laplace_distribution (const param_type &parm) :
+        _location(parm.location()), _scale(parm.scale())
     {
-        assert(scale_ >= 0);
+        BOOST_ASSERT(_scale > RealType(0.0));
     }
 
-    RealType min () const {return -std::numeric_limits<RealType>::infinity();}
-    RealType max () const {return std::numeric_limits<RealType>::infinity();}
-    RealType location () const {return location_;}
-    RealType scale () const {return scale_;}
-    param_type param () const {return param_type(location_, scale_);}
-    void param (const param_type &param)
+    /** Returns the location of the distribution. */
+    RealType location () const {return _location;}
+
+    /** Returns the scale of the distribution. */
+    RealType scale () const {return _scale;}
+
+    /** Returns the smallest value that the distribution can produce. */
+    RealType min BOOST_PREVENT_MACRO_SUBSTITUTION () const
+    {return -std::numeric_limits<RealType>::infinity();}
+
+    /** Returns the largest value that the distribution can produce. */
+    RealType max BOOST_PREVENT_MACRO_SUBSTITUTION () const
+    {return std::numeric_limits<RealType>::infinity();}
+
+    /** Returns the parameters of the distribution. */
+    param_type param () const {return param_type(_location, _scale);}
+
+    /** Sets the parameters of the distribution. */
+    void param (const param_type &parm)
     {
-        location_ = param.location();
-        scale_ = param.scale();
+        _location = parm.location();
+        _scale = parm.scale();
     }
 
+    /**
+     * Effects: Subsequent uses of the distribution do not depend
+     * on values produced by any engine prior to invoking reset.
+     */
     void reset () {}
 
+    /** Returns a laplace variate. */
     template <typename Engine>
     result_type operator() (Engine &eng)
     {
+        using std::abs;
+
         RealType u = boost::random::uniform_01<RealType>()(eng) - 0.5;
         RealType r = -2 * std::abs(u);
 
         r = boost::math::log1p(r);
         r = u > 0 ? r : -r;
 
-        return location_ - scale_ * r;
+        return _location - _scale * r;
     }
 
-    template <typename Engine>
-    result_type operator() (Eigen &eng, const param_type &param)
+    /** Returns a laplace variate with parameters specified by @c param. */
+    template <typename URNG>
+    result_type operator() (URNG &urng, const param_type &parm)
     {
-        return laplace_distribution(param)(eng);
+        return laplace_distribution(parm)(urng);
     }
+
+    /** Writes a @c laplace_distribution to a @c std::ostream. */
+    BOOST_RANDOM_DETAIL_OSTREAM_OPERATOR(os, laplace_distribution, nd)
+    {
+        os << nd._location << " " << nd._scale << " ";
+        return os;
+    }
+
+    /** Reads a @c laplace_distribution from a @c std::istream. */
+    BOOST_RANDOM_DETAIL_ISTREAM_OPERATOR(is, laplace_distribution, nd)
+    {
+        is >> std::ws >> nd._location >> std::ws >> nd._scale;
+        return is;
+    }
+
+    /**
+     * Returns true if the two instances of @c laplace_distribution will
+     * return identical sequences of values given equal generators.
+     */
+    BOOST_RANDOM_DETAIL_EQUALITY_OPERATOR(laplace_distribution, lhs, rhs)
+    {
+        return lhs._location == rhs._location && lhs._scale == rhs._scale;
+    }
+
+    /**
+     * Returns true if the two instances of @c laplace_distribution will
+     * return different sequences of values given equal generators.
+     */
+    BOOST_RANDOM_DETAIL_INEQUALITY_OPERATOR(laplace_distribution)
 
     private :
 
-    RealType location_;
-    RealType scale_;
+    RealType _location;
+    RealType _scale;
 }; // class laplace_distribution
 
-} // namespace vSMC
+} // namespace random
 
-#endif // V_SMC_RNG_LAPLACE_DISTRIBUTION_HPP
+using random::laplace_distribution;
+
+} // namespace boost
+
+#endif // BOOST_RANDOM_LAPLACE_DISTRIBUTION_HPP
