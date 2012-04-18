4ef097894e945b62ad80cb373c775537ad78619b
Yan Zhou <zhouyan@me.com>
2012-04-18 12:08:37 +0100

    it is now possible to use tr1 function and random

--- include/vSMC/internal/function.hpp
+++ include/vSMC/internal/function.hpp
@@ -1,13 +1,28 @@
 #include <vSMC/internal/config.hpp>
 
 #ifdef V_SMC_USE_STD_FUNCTION
+
 #include <functional>
 namespace vSMC { namespace internal {
 using std::function;
 } }
+
 #else // V_SMC_USE_STD_FUNCTION
+
+#ifdef V_SMC_USE_TR1_FUNCTION
+
+#include <tr1/functional>
+namespace vSMC { namespace internal {
+using std::tr1::function;
+} }
+
+#else // V_SMC_USE_TR1_FUNCTION
+
 #include <boost/function.hpp>
 namespace vSMC { namespace internal {
 using boost::function;
 } }
+
+#endif // V_SMC_USE_TR1_FUNCTION
+
 #endif // V_SMC_USE_STD_FUNCTION
--- include/vSMC/internal/random.hpp
+++ include/vSMC/internal/random.hpp
@@ -3,7 +3,6 @@
 #ifdef V_SMC_USE_STD_RANDOM
 
 #include <random>
-
 namespace vSMC { namespace internal {
 using std::binomial_distribution;
 using std::uniform_real_distribution;
@@ -11,16 +10,38 @@ using std::uniform_real_distribution;
 
 #else // V_SMC_USE_STD_RANDOM
 
-#include <boost/random/binomial_distribution.hpp>
-#include <boost/random/uniform_real_distribution.hpp>
+#ifdef V_SMC_USE_TR1_RANDOM
+
+#include <tr1/random>
 namespace vSMC { namespace internal {
+using std::tr1::binomial_distribution;
+template <typename RealType = double>
+class uniform_real_distribution : public std::tr1::uniform_real<RealType>
+{
+    public :
+
+    explicit uniform_real_distribution (
+            RealType min_arg = RealType(0),
+            RealType max_arg = RealType(1)):
+        std::tr1::uniform_real<RealType>(min_arg, max_arg) {}
+}; // class uniform_real_distribution
+} }
+
+#else // V_SMC_USE_TR1_RANDOM
+
+#include <boost/random.hpp>
 #if BOOST_VERSION < 104700
+namespace vSMC { namespace internal {
 using boost::binomial_distribution;
 using boost::uniform_real_distribution;
+} }
 #else // BOOST_VERSION < 104700
+namespace vSMC { namespace internal {
 using boost::random::binomial_distribution;
 using boost::random::uniform_real_distribution;
-#endif // BOOST_VERSION < 104700
 } }
+#endif // BOOST_VERSION < 104700
+
+#endif // V_SMC_USE_TR1_RANDOM
  
 #endif // V_SMC_USE_STD_RANDOM
