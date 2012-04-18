d13e7ef619feee4809a61ebee25998f3bbb5a4c6
Yan Zhou <zhouyan@me.com>
2012-04-18 21:28:16 +0100

    try more rng

--- cmake/try_random.cpp
+++ cmake/try_random.cpp
@@ -5,11 +5,28 @@
 int main ()
 {
     r123::Engine<r123::Threefry4x64> eng(101);
-    vSMC::internal::binomial_distribution<> rbinom(100, 2);
     vSMC::internal::uniform_real_distribution<> runif(0, 1);
 
-    int x = rbinom(eng);
-    double y = runif(eng);
+    int N = 10000000;
+    int size = 20;
+    double p = 0;
+    std::cout.precision(15);
+    while (p < 0.2 || p > 0.8)
+        p = runif(eng);
+    std::cout << "p: " << p << std::endl;
+
+    int *y = new int[size + 1];
+    for (int i = 0; i != size + 1; ++i)
+        y[i] = 0;
+
+    vSMC::internal::binomial_distribution<> rbinom(size, p);
+    for (int i = 0; i != N; ++i)
+        ++y[rbinom(eng)];
+
+    for (int i = 0; i != size + 1; ++i)
+        std::cout <<  y[i] / static_cast<double>(N) << std::endl;
+
+    delete [] y;
 
     return 0;
 }
