600f9a967254e588a684076f8f15df131f25b53d
Yan Zhou <zhouyan@me.com>
2012-04-18 21:49:29 +0100

    assert generated rng in range

--- cmake/try_random.cpp
+++ cmake/try_random.cpp
@@ -1,32 +1,31 @@
+#include <cassert>
 #include <Random123/threefry.h>
 #include <Random123/conventional/Engine.hpp>
 #include <vSMC/internal/random.hpp>
 
 int main ()
 {
+    int N = 1000000;
     r123::Engine<r123::Threefry4x64> eng(101);
-    vSMC::internal::uniform_real_distribution<> runif(0, 1);
 
-    int N = 10000000;
-    int size = 20;
+    vSMC::internal::uniform_real_distribution<> runif(0, 1);
     double p = 0;
-    std::cout.precision(15);
+    double sum = 0;
+    for (int i = 0; i != N; ++i) {
+        p = runif(eng);
+        assert(p >= 0 && p <= 1);
+    }
+
     while (p < 0.2 || p > 0.8)
         p = runif(eng);
-    std::cout << "p: " << p << std::endl;
 
-    int *y = new int[size + 1];
-    for (int i = 0; i != size + 1; ++i)
-        y[i] = 0;
-
+    int size = 20;
+    int res = 0;
     vSMC::internal::binomial_distribution<> rbinom(size, p);
-    for (int i = 0; i != N; ++i)
-        ++y[rbinom(eng)];
-
-    for (int i = 0; i != size + 1; ++i)
-        std::cout <<  y[i] / static_cast<double>(N) << std::endl;
-
-    delete [] y;
+    for (int i = 0; i != N; ++i) {
+        res = rbinom(eng);
+        assert(res >= 0 && res <= size);
+    }
 
     return 0;
 }
