6ec42474f755a473e4355e0321dd0ff04fed657f
Yan Zhou <zhouyan@me.com>
2012-04-19 23:56:11 +0100

    Conditional include TBB headers

--- include/vSMC.hpp
+++ include/vSMC.hpp
@@ -2,7 +2,11 @@
 #define V_SMC_HPP
 
 #include <vSMC/core/sampler.hpp>
+
 #include <vSMC/helper/sequential.hpp>
+
+#ifdef V_SMC_USE_TBB
 #include <vSMC/helper/parallel_tbb.hpp>
+#endif
 
 #endif // V_SMC_HPP
