b9c0ddefe20cf2f0ebf0ae87d9cc5b2770d76a8e
Yan Zhou <zhouyan@me.com>
2012-04-21 12:23:42 +0100

    use default seed and engine

--- cmake/try_random.cpp
+++ cmake/try_random.cpp
@@ -1,12 +1,10 @@
 #include <cassert>
-#include <Random123/threefry.h>
-#include <Random123/conventional/Engine.hpp>
 #include <vSMC/internal/random.hpp>
 
 int main ()
 {
-    int N = 1000;
-    r123::Engine<r123::Threefry4x64> eng(101);
+    int N = 100000;
+    r123::Engine<V_SMC_CRNG_TYPE> eng(V_SMC_CRNG_SEED);
 
     vSMC::internal::uniform_real_distribution<> runif(0, 1);
     for (int i = 0; i != N; ++i) {
