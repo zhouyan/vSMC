8f9398c26ff700703d355c8667dbdb8288a339b7
Yan Zhou <zhouyan@me.com>
2012-04-21 18:49:09 +0100

    add comments in configure file

--- include/vSMC/internal/config.hpp
+++ include/vSMC/internal/config.hpp
@@ -1,6 +1,12 @@
 #ifndef V_SMC_CONFIG_CONFIG_HPP
 #define V_SMC_CONFIG_CONFIG_HPP
 
+// Uncomment the following line to use C++11 <functional>
+// #define V_SMC_USE_STD_FUNCTION
+
+// Uncomment the following line to use C++11 <random>
+// #define V_SMC_USE_STD_RANDOM
+
 #if !defined(V_SMC_USE_STD_FUNCITON) || !defined(V_SMC_USE_STD_RANDOM)
 #include <boost/version.hpp>
 #ifndef V_SMC_NDEBUG
