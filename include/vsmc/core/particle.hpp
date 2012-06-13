3ae48f86a952e187bcddc8b0904b2fe7026fe279
Yan Zhou <zhouyan@me.com>
2012-06-13 21:59:26 +0100

    add and set log weights support iterator input

--- include/vsmc/core/particle.hpp
+++ include/vsmc/core/particle.hpp
@@ -49,9 +49,10 @@ class Particle
     ///
     /// \post All weights are initialized to be euqal to each other
     explicit Particle (size_type N, seed_type seed = VSMC_RNG_SEED) :
-        size_(N), value_(N), weight_(N), weight_cached_(false),
-        log_weight_(N), inc_weight_(N), replication_(N),
-        ess_(N), ess_cached_(false), resampled_(false), zconst_(0), seed_(seed)
+        size_(N), value_(N), ess_(N),
+        weight_(N), log_weight_(N), inc_weight_(N), replication_(N),
+        ess_cached_(false), weight_cached_(false), log_weight_cached_(false),
+        resampled_(false), zconst_(0), seed_(seed)
 #ifndef VSMC_USE_SEQUENTIAL_RNG
         , prng_(N)
 #endif
@@ -82,9 +83,7 @@ class Particle
     const weight_type &weight () const
     {
         if (!weight_cached_) {
-            double max_weight = log_weight_.maxCoeff();
-            log_weight_ = log_weight_.array() - max_weight;
-            weight_ = log_weight_.array().exp();
+            weight_ = log_weight().array().exp();
             double sum = weight_.sum();
             weight_ *= 1 / sum;
             weight_cached_ = true;
@@ -96,23 +95,46 @@ class Particle
     /// Read only access to the log weights
     const weight_type &log_weight () const
     {
+        if (!log_weight_cached_) {
+            double max_weight = log_weight_.maxCoeff();
+            log_weight_ = log_weight_.array() - max_weight;
+            log_weight_cached_ = true;
+        }
+
         return log_weight_;
     }
 
     /// Set equal weights for all particles
     void set_equal_weight ()
     {
-        log_weight_.setConstant(0);
-        weight_.setConstant(1.0 / size_);
-        weight_cached_ = true;
         ess_ = size_;
+        weight_.setConstant(1.0 / size_);
+        log_weight_.setConstant(0);
+
         ess_cached_ = true;
+        weight_cached_ = true;
+        log_weight_cached_ = true;
     }
 
-    /// \brief Set the log weights with a pointer
+    /// \brief Set the log weights with an iterator
     ///
-    /// \param new_weight New log weights
-    /// \param delta A multiplier appiled to new_weight
+    /// \param first The position to start the reading, it shall be valid after
+    /// increments of size() times.
+    /// \param delta A multiplier appiled to the new log weights
+    template <typename InputIter>
+    void set_log_weight (InputIter first, double delta = 1)
+    {
+        size_type i = 0;
+        while (i != size_) {
+            log_weight_[i] = *first;
+            ++first;
+            ++i;
+        }
+        log_weight_ *= delta;
+        set_log_weight();
+    }
+
+    /// Set the log weights with a pointer
     void set_log_weight (const double *new_weight, double delta = 1)
     {
         Eigen::Map<const weight_type> w(new_weight, size_);
@@ -126,12 +148,33 @@ class Particle
         set_log_weight();
     }
 
-    /// \brief Add to the log weights with a pointer
+    /// \brief Add to the log weights with an iterator
     ///
-    /// \param inc_weight Incremental log weights
-    /// \param delta A multiplier applied to inc_weight
-    /// \param add_zconst Whether this incremental weights should contribute
-    /// the esitmates of normalizing constants
+    /// \param first The position to start the reading, it shall be valid after
+    /// increments of size() times.
+    /// \param delta A multiplier appiled to the new incremental log weights
+    /// \param add_zconst Whether this incremental weights shall contribute to
+    /// the SMC normalizing constant estimate
+    template <typename InputIter>
+    void add_log_weight (InputIter first, double delta = 1,
+            bool add_zconst = true)
+    {
+        using std::log;
+
+        size_type i = 0;
+        while (i != size_) {
+            inc_weight_[i] = *first;
+            ++first;
+            ++i;
+        }
+        inc_weight_ *= delta;
+        log_weight_ += inc_weight_;
+        if (add_zconst)
+            zconst_ += log(weight().dot(inc_weight_.array().exp().matrix()));
+        set_log_weight();
+    }
+
+    /// Add to the log weights with a pointer
     void add_log_weight (const double *inc_weight, double delta = 1,
             bool add_zconst = true)
     {
@@ -145,11 +188,10 @@ class Particle
     {
         using std::log;
 
-        if (add_zconst) {
-            inc_weight_ = (delta * inc_weight.head(size_)).array().exp();
-            zconst_ += log(weight_.dot(inc_weight_.head(size_)));
-        }
-        log_weight_ += delta * inc_weight.head(size_);
+        inc_weight_ = delta * inc_weight.head(size_);
+        log_weight_ += inc_weight_;
+        if (add_zconst)
+            zconst_ += log(weight().dot(inc_weight_.array().exp().matrix()));
         set_log_weight();
     }
 
@@ -189,6 +231,9 @@ class Particle
     /// \param threshold The threshold for resampling
     void resample (ResampleScheme scheme, double threshold)
     {
+        assert(replication_.size() == size());
+
+        // call to ess() will normalize weight first
         resampled_ = ess() < threshold * size_;
 
         if (resampled_) {
@@ -259,15 +304,16 @@ class Particle
 
     size_type size_;
     value_type value_;
+    mutable double ess_;
 
     mutable weight_type weight_;
-    mutable bool weight_cached_;
     mutable weight_type log_weight_;
     mutable weight_type inc_weight_;
     mutable replication_type replication_;
 
-    mutable double ess_;
     mutable bool ess_cached_;
+    mutable bool weight_cached_;
+    mutable bool log_weight_cached_;
 
     bool resampled_;
     double zconst_;
@@ -281,8 +327,13 @@ class Particle
 
     void set_log_weight ()
     {
-        weight_cached_ = false;
         ess_cached_ = false;
+        weight_cached_ = false;
+        log_weight_cached_ = false;
+
+        assert(weight_.size() == size_);
+        assert(log_weight_.size() == size_);
+        assert(inc_weight_.size() == size_);
     }
 
     void resample_multinomial ()
@@ -296,6 +347,7 @@ class Particle
         /// fractional part of N * weight. log_weight: act as the integral
         /// part of N * weight.  They all will be reset to equal weights after
         /// resampling.  So it is safe to modify them here.
+
         using std::modf;
 
         for (size_type i = 0; i != size_; ++i)
