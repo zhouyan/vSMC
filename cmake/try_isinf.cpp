52fc179dbf4f81cc7831c848804dff62ad9594b3
Yan Zhou <zhouyan@me.com>
2012-06-27 23:48:59 +0100

    fix isfinite check

--- cmake/try_isinf.cpp
+++ cmake/try_isinf.cpp
@@ -2,20 +2,17 @@
 #include <limits>
 #include <cmath>
 
-#ifdef VSMC_USE_STD_FPCLASSIFY
+#ifdef VSMC_USE_STD_ISINF
 using std::isinf;
-using std::isfinite;
-#elif defined(VSMC_USE_BOOST_FPCLASSIFY)
+#elif defined(VSMC_USE_BOOST_ISINF)
 #include <boost/math/special_functions/fpclassify.hpp>
 using boost::math::isinf;
-using boost::math::isfinite;
 #endif
 
 int main ()
 {
     double x =  std::numeric_limits<double>::infinity();
     assert(isinf(x));
-    assert(!isfinite(x));
 
     return 0;
 }
