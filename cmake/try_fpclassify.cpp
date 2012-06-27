87532b438e6dc0fa5a882176c1d7186c098798c7
Yan Zhou <zhouyan@me.com>
2012-06-27 21:53:46 +0100

    add isfinite to fpclassify test

--- cmake/try_fpclassify.cpp
+++ cmake/try_fpclassify.cpp
@@ -2,17 +2,20 @@
 #include <limits>
 #include <cmath>
 
-#ifdef VSMC_USE_STD_ISINF
+#ifdef VSMC_USE_STD_FPCLASSIFY
 using std::isinf;
-#elif defined(VSMC_USE_BOOST_ISINF)
+using std::isfinite;
+#elif defined(VSMC_USE_BOOST_FPCLASSIFY)
 #include <boost/math/special_functions/fpclassify.hpp>
 using boost::math::isinf;
+using boost::math::isfinite;
 #endif
 
 int main ()
 {
     double x =  std::numeric_limits<double>::infinity();
     assert(isinf(x));
+    assert(!isfinite(x));
 
     return 0;
 }
