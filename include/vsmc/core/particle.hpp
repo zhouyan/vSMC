d68ad204e726f93a82d9098f21127abb9ec8652b
Yan Zhou <zhouyan@me.com>
2012-07-19 13:18:48 +0100

    Revert "Revert "Replace valarray with vector""
    This reverts commit 14ce7fccf3789423446ff5344c922fc226f73e49.

--- include/vsmc/core/particle.hpp
+++ include/vsmc/core/particle.hpp
@@ -218,9 +218,9 @@ class Particle :
     size_type size_;
     value_type value_;
 
-    std::valarray<size_type> replication_;
-    std::valarray<size_type> copy_from_;
-    std::valarray<double> weight_;
+    std::vector<size_type> replication_;
+    std::vector<size_type> copy_from_;
+    std::vector<double> weight_;
     bool resampled_;
     resample_op_type resample_op_;
 
@@ -228,10 +228,11 @@ class Particle :
     {
         // Some times the nuemrical round error can cause the total childs
         // differ from number of particles
-        size_type sum = replication_.sum();
+        size_type sum = std::accumulate(
+                replication_.begin(), replication_.end(), 0);
         if (sum != size_) {
-            size_type *id_max = std::max_element(
-                    &replication_[0], &replication_[0] + size_);
+            typename std::vector<size_type>::iterator id_max =
+                std::max_element(replication_.begin(), replication_.end());
             *id_max += size_ - sum;
         }
 
