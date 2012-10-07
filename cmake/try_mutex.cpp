8e76519a438b4c025bbff19201224e26eb624ec6
Yan Zhou <zhouyan@me.com>
2012-10-07 17:14:54 +0100

    add mutex test

---
+++ cmake/try_mutex.cpp
@@ -0,0 +1,24 @@
+#include <vsmc/cxx11/mutex.hpp>
+
+vsmc::cxx11::mutex mtx;
+int mtx_d;
+
+void op1 ()
+{
+    vsmc::cxx11::lock_guard<vsmc::cxx11::mutex> guard(mtx);
+    mtx_d = 1;
+}
+
+void op2 ()
+{
+    vsmc::cxx11::lock_guard<vsmc::cxx11::mutex> guard(mtx);
+    mtx_d = 2;
+}
+
+int main ()
+{
+    op1();
+    op2();
+
+    return 0;
+}
