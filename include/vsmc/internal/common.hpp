1d82fc5f16f1002befc5f0e5a7e5ab482fd2f32a
Yan Zhou <zhouyan@me.com>
2012-10-08 14:22:09 +0100

    include additional c++ 11 headers

--- include/vsmc/internal/common.hpp
+++ include/vsmc/internal/common.hpp
@@ -28,6 +28,18 @@
 #include <vsmc/cxx11/random.hpp>
 #include <vsmc/cxx11/type_traits.hpp>
 
+#if VSMC_USE_CHRONO
+#include <vsmc/cxx11/chrono.hpp>
+#endif
+
+#if VSMC_USE_MUTEX
+#include <vsmc/cxx11/mutex.hpp>
+#endif
+
+#if VSMC_USE_THREAD
+#include <vsmc/cxx11/thread.hpp>
+#endif
+
 #ifdef NDEBUG
 #define VSMC_RUNTIME_ASSERT(cond, msg)
 #else // NDEBUG
