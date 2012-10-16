d2f0e01948ade7bb5ac8bc7df45a8d7b0e7b19f0
Yan Zhou <zhouyan@me.com>
2012-10-16 07:46:57 +0100

    add header for particle

--- include/vsmc_capi.h
+++ include/vsmc_capi.h
@@ -36,22 +36,47 @@ typedef struct
     int base_type;
 } vsmcParticleInfo;
 
-/* vsmc::Sampler */
+typedef struct
+{
+    void *monitor_ptr;
+    int base_type;
+} vsmcMonitorInfo;
 
-vsmcSamplerInfo vsmc_sampler_new (size_t, unsigned, int, double, int);
-void vsmc_sampler_delete                 (vsmcSamplerInfo);
-size_t vsmc_sampler_size                 (vsmcSamplerInfo);
-unsigned vsmc_sampler_iter_size          (vsmcSamplerInfo);
-void vsmc_sampler_resample_scheme        (vsmcSamplerInfo, int);
-double vsmc_sampler_resample_threshold   (vsmcSamplerInfo, double);
-void vsmc_sampler_read_ess_history       (vsmcSamplerInfo, double *);
-void vsmc_sampler_read_resampled_history (vsmcSamplerInfo, int *);
-void vsmc_sampler_initialize             (vsmcSamplerInfo, void *);
-void vsmc_sampler_iterate                (vsmcSamplerInfo, unsigned);
-void vsmc_sampler_show_progress          (vsmcSamplerInfo, int);
-vsmcParticleInfo vsmc_sampler_particle   (vsmcSamplerInfo);
+typedef struct
+{
+    void *monitor_ptr;
+    int base_type;
+} vsmcPathInfo;
+
+/* vsmc::Sampler */
+vsmcSamplerInfo  vsmc_sampler_new (size_t, unsigned, int, double, int);
+vsmcParticleInfo vsmc_sampler_particle (vsmcSamplerInfo);
+void     vsmc_sampler_delete                 (vsmcSamplerInfo);
+size_t   vsmc_sampler_size                   (vsmcSamplerInfo);
+unsigned vsmc_sampler_iter_size              (vsmcSamplerInfo);
+void     vsmc_sampler_resample_scheme        (vsmcSamplerInfo, int);
+double   vsmc_sampler_resample_threshold     (vsmcSamplerInfo, double);
+void     vsmc_sampler_read_ess_history       (vsmcSamplerInfo, double *);
+void     vsmc_sampler_read_resampled_history (vsmcSamplerInfo, int *);
+void     vsmc_sampler_initialize             (vsmcSamplerInfo, void *);
+void     vsmc_sampler_iterate                (vsmcSamplerInfo, unsigned);
+void     vsmc_sampler_show_progress          (vsmcSamplerInfo, int);
 
 /* vsmc::Particle */
+size_t vsmc_particle_size             (vsmcParticleInfo)
+void   vsmc_particle_resample         (vsmcParticleInfo, double)
+int    vsmc_particle_resampled        (vsmcParticleInfo)
+void   vsmc_particle_resample_scheme  (vsmcParticleInfo, int)
+void   vsmc_particle_read_weight      (vsmcParticleInfo, double *)
+void   vsmc_particle_read_log_weight  (vsmcParticleInfo, double *)
+double vsmc_particle_weight           (vsmcParticleInfo, size_t)
+double vsmc_particle_log_weight       (vsmcParticleInfo, size_t)
+void   vsmc_particle_set_equal_weight (vsmcParticleInfo)
+void   vsmc_particle_set_weight       (vsmcParticleInfo, const double *)
+void   vsmc_particle_mul_weight       (vsmcParticleInfo, const double *)
+void   vsmc_particle_set_log_weight   (vsmcParticleInfo, const double *)
+void   vsmc_particle_add_log_weight   (vsmcParticleInfo, const double *)
+double vsmc_particle_ess              (vsmcParticleInfo info)
 
 #ifdef __cplusplus
 }
