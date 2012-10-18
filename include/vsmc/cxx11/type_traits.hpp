7e83d15adb723318c762df104dc01da9da7907a9
Yan Zhou <zhouyan@me.com>
2012-10-18 23:55:43 +0100

    configure defaults to only include used names in chrono

--- include/vsmc/cxx11/type_traits.hpp
+++ include/vsmc/cxx11/type_traits.hpp
@@ -6,15 +6,20 @@
 #if VSMC_HAS_CXX11LIB_TYPE_TRAITS
 #include <type_traits>
 namespace vsmc { namespace cxx11 {
-#if VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 using std::integral_constant;
 using std::true_type;
 using std::false_type;
+using std::is_base_of;
+using std::is_pointer;
+using std::is_same;
+using std::make_signed;
+using std::remove_cv;
+using std::remove_pointer;
+#if VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 using std::is_void;
 using std::is_integral;
 using std::is_floating_point;
 using std::is_array;
-using std::is_pointer;
 using std::is_lvalue_reference;
 using std::is_rvalue_reference;
 using std::is_member_object_pointer;
@@ -70,23 +75,18 @@ using std::has_virtual_destructor;
 using std::alignment_of;
 using std::rank;
 using std::extent;
-using std::is_same;
-using std::is_base_of;
 using std::is_convertible;
 using std::remove_const;
 using std::remove_volatile;
-using std::remove_cv;
 using std::add_const;
 using std::add_volatile;
 using std::add_cv;
 using std::remove_reference;
 using std::add_lvalue_reference;
 using std::add_rvalue_reference;
-using std::make_signed;
 using std::make_unsigned;
 using std::remove_extent;
 using std::remove_all_extents;
-using std::remove_pointer;
 using std::add_pointer;
 using std::aligned_storage;
 using std::decay;
@@ -95,30 +95,25 @@ using std::conditional;
 using std::common_type;
 using std::underlying_type;
 using std::result_of;
-#else // VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
-using std::integral_constant;
-using std::true_type;
-using std::false_type;
-using std::is_base_of;
-using std::is_pointer;
-using std::is_same;
-using std::make_signed;
-using std::remove_cv;
-using std::remove_pointer;
 #endif // VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 } }
 #else // VSMC_HAS_CXX11LIB_TYPE_TRAITS
 #include <boost/type_traits.hpp>
 namespace vsmc { namespace cxx11 {
-#if VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 using boost::integral_constant;
 using boost::true_type;
 using boost::false_type;
+using boost::is_base_of;
+using boost::is_pointer;
+using boost::is_same;
+using boost::make_signed;
+using boost::remove_cv;
+using boost::remove_pointer;
+#if VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 using boost::is_void;
 using boost::is_integral;
 using boost::is_floating_point;
 using boost::is_array;
-using boost::is_pointer;
 using boost::is_lvalue_reference;
 using boost::is_rvalue_reference;
 using boost::is_member_object_pointer;
@@ -174,23 +169,18 @@ using boost::has_virtual_destructor;
 using boost::alignment_of;
 using boost::rank;
 using boost::extent;
-using boost::is_same;
-using boost::is_base_of;
 using boost::is_convertible;
 using boost::remove_const;
 using boost::remove_volatile;
-using boost::remove_cv;
 using boost::add_const;
 using boost::add_volatile;
 using boost::add_cv;
 using boost::remove_reference;
 using boost::add_lvalue_reference;
 using boost::add_rvalue_reference;
-using boost::make_signed;
 using boost::make_unsigned;
 using boost::remove_extent;
 using boost::remove_all_extents;
-using boost::remove_pointer;
 using boost::add_pointer;
 using boost::aligned_storage;
 using boost::decay;
@@ -199,16 +189,6 @@ using boost::conditional;
 using boost::common_type;
 // using boost::underlying_type;
 // using boost::result_of;
-#else // VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
-using boost::integral_constant;
-using boost::true_type;
-using boost::false_type;
-using boost::is_base_of;
-using boost::is_pointer;
-using boost::is_same;
-using boost::make_signed;
-using boost::remove_cv;
-using boost::remove_pointer;
 #endif // VSMC_HAS_LIB_TYPE_TRAITS_COMPLETE
 } }
 #endif // VSMC_HAS_CXX11LIB_TYPE_TRAITS
