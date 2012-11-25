ab81443aea7927ba6d8dc9b99c0700c70d499b9f
Yan Zhou <zhouyan@me.com>
2012-11-25 23:41:37 +0000

    StaticFor::apply op accept iterator as arguments

--- include/vsmc/utility/meta.hpp
+++ include/vsmc/utility/meta.hpp
@@ -1,6 +1,8 @@
 #ifndef VSMC_UTILITY_META_HPP
 #define VSMC_UTILITY_META_HPP
 
+#include <vsmc/internal/common.hpp>
+
 namespace vsmc {
 
 template <unsigned Dim>
@@ -9,14 +11,14 @@ struct StaticFor
     template <typename Iter, typename Op>
     static void apply (Iter first, Op op)
     {
-	op(*first);
+	op(first);
 	StaticFor<Dim - 1>::apply(++first, op);
     }
 
     template <typename Iter1, typename Iter2, typename Op>
     static void apply (Iter1 first1, Iter2 first2, Op op)
     {
-	op(*first1, *first2);
+	op(first1, first2);
 	StaticFor<Dim - 1>::apply(++first1, ++first2, op);
     }
 }; // struct StaticFor
@@ -27,13 +29,13 @@ struct StaticFor<1U>
     template <typename Iter, typename UnaryOp>
     static void apply (Iter first, UnaryOp op)
     {
-	op(*first);
+	op(first);
     }
 
     template <typename Iter1, typename Iter2, typename Op>
     static void apply (Iter1 first1, Iter2 first2, Op op)
     {
-	op(*first1, *first2);
+	op(first1, first2);
     }
 }; // struct StaticFor
 
