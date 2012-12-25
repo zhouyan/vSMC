13ad4c85b141bfe976a520d4ef7b28a386fa847d
Yan Zhou <zhouyan@me.com>
2012-12-25 15:59:20 +0800

    fast normal

--- include/vsmc/utility/random/normal01.h
+++ include/vsmc/utility/random/normal01.h
@@ -9,15 +9,12 @@
         CBRNG4x##W##_CTR_T *c;                                               \
         CBRNG4x##W##_CTR_T r;                                                \
         FT u[4];                                                             \
-        char remain;                                                         \
+        unsigned char remain;                                                \
     } normal01_##W##_##F;                                                    \
                                                                              \
-    VSMC_STATIC_INLINE void normal01_##W##_##F##_init (                      \
-            normal01_##W##_##F *rnorm,                                       \
-            CBRNG4x##W##_KEY_T *k, CBRNG4x##W##_CTR_T *c)                    \
+    VSMC_STATIC_INLINE void normal01_##W##_##F##_step (                      \
+            normal01_##W##_##F *rnorm)                                       \
     {                                                                        \
-        rnorm->k = k;                                                        \
-        rnorm->c = c;                                                        \
         rnorm->c->v[0]++;                                                    \
         rnorm->r = CBRNG4x##W(*(rnorm->c), *(rnorm->k));                     \
         rnorm->u[0] = u01_open_closed_##W##_##F(rnorm->r.v[0]);              \
@@ -27,36 +24,27 @@
         rnorm->remain = 4;                                                   \
     }                                                                        \
                                                                              \
+    VSMC_STATIC_INLINE void normal01_##W##_##F##_init (                      \
+            normal01_##W##_##F *rnorm,                                       \
+            CBRNG4x##W##_KEY_T *k, CBRNG4x##W##_CTR_T *c)                    \
+    {                                                                        \
+        rnorm->k = k;                                                        \
+        rnorm->c = c;                                                        \
+        normal01_##W##_##F##_step(rnorm);                                    \
+    }                                                                        \
+                                                                             \
     VSMC_STATIC_INLINE FT normal01_##W##_##F##_rand (                        \
             normal01_##W##_##F *rnorm)                                       \
     {                                                                        \
-        if (!rnorm->remain) {                                                \
-            rnorm->c->v[0]++;                                                \
-            rnorm->r = CBRNG4x##W(*(rnorm->c), *(rnorm->k));                 \
-            rnorm->u[0] = u01_open_closed_##W##_##F(rnorm->r.v[0]);          \
-            rnorm->u[1] = u01_open_closed_##W##_##F(rnorm->r.v[1]);          \
-            rnorm->u[2] = u01_open_closed_##W##_##F(rnorm->r.v[2]);          \
-            rnorm->u[3] = u01_open_closed_##W##_##F(rnorm->r.v[3]);          \
-            rnorm->remain = 4;                                               \
-        }                                                                    \
+        if (!rnorm->remain)                                                  \
+            normal01_##W##_##F##_step(rnorm);                                \
                                                                              \
         rnorm->remain--;                                                     \
-        switch (rnorm->remain) {                                             \
-            case 0 :                                                         \
-                return sqrt(-2 * log(rnorm->u[0])) *                         \
-                    cos(2 * M_PI_##F * rnorm->u[1]);                         \
-            case 1 :                                                         \
-                return sqrt(-2 * log(rnorm->u[0])) *                         \
-                    sin(2 * M_PI_##F * rnorm->u[1]);                         \
-            case 2 :                                                         \
-                return sqrt(-2 * log(rnorm->u[2])) *                         \
-                    cos(2 * M_PI_##F * rnorm->u[3]);                         \
-            case 3 :                                                         \
-                return sqrt(-2 * log(rnorm->u[2])) *                         \
-                    sin(2 * M_PI_##F * rnorm->u[3]);                         \
-            default :                                                        \
-                return 0;                                                    \
-        }                                                                    \
+        unsigned char k = rnorm->remain;                                     \
+        unsigned char i = (k>>1)<<1;                                         \
+        return k == i ?                                                      \
+            sqrt(-2 * log(rnorm->u[i])) * cos(2 * M_PI_##F * rnorm->u[i+1]): \
+            sqrt(-2 * log(rnorm->u[i])) * sin(2 * M_PI_##F * rnorm->u[i+1]); \
     }
 
 VSMC_DEFINE_NORMAL01(32, 24, float);
