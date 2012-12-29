6aa9c7b91fe33113327dfb35f18a62a873dfd878
Yan Zhou <zhouyan@me.com>
2012-12-29 21:38:16 +0800

    suppress ealier gcc warning

--- include/vsmc/utility/opencl/cl_wrapper.hpp
+++ include/vsmc/utility/opencl/cl_wrapper.hpp
@@ -2,14 +2,18 @@
 #define VSMC_UTILITY_OPENCL_CL_WRAPPER_HPP
 
 #if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#if __GNUC__ >= 4 && __GNUC_MINIOR__ >= 6
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 #endif
 
 #include <cl.hpp>
 
 #if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#if __GNUC__ >= 4 && __GNUC_MINIOR__ >= 6
 #pragma GCC diagnostic pop
+#endif
 #endif
 
 #endif // VSMC_UTILITY_OPENCL_CL_WRAPPER_HPP
