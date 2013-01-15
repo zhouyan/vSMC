fd824c6ae0b4882404087460207be0417dacab20
Yan Zhou <zhouyan@me.com>
2013-01-15 18:14:49 +0000

    implement Mac stop watch

--- include/vsmc/utility/stop_watch.hpp
+++ include/vsmc/utility/stop_watch.hpp
@@ -84,22 +84,85 @@ class StopWatch
 #elif defined(__APPLE__) || defined(__MACOSX)
 #define VSMC_STOP_WATCH_DEFINED
 
+#include <mach/mach_time.h>
+
 namespace vsmc {
 
 class StopWatch
 {
     public :
 
-    void start () const {}
-    void stop () const {}
-    void reset () const {}
+    StopWatch ()
+    {
+        reset();
+    }
 
-    double nanoseconds () const { return 0; }
-    double microseconds () const { return 0; }
-    double milliseconds () const { return 0; }
-    double seconds () const { return 0; }
-    double minutes () const { return 0; }
-    double hours () const { return 0; }
+    void start () const
+    {
+        start_time_ = mach_absolute_time();
+    }
+
+    void stop () const
+    {
+        uint64_t stop_time = mach_absolute_time();
+        uint64_t elapsed_abs = stop_time - start_time_;
+        uint64_t elapsed_nano = elapsed_abs *
+            timebase_.numer / timebase_.denom;
+        uint64_t sec = elapsed_nano / ratio_;
+        elapsed_sec_ += sec;
+        elapsed_nsec_ += elapsed_nano - sec * ratio_;
+    }
+
+    void reset () const
+    {
+        elapsed_sec_ = 0;
+        elapsed_nsec_ = 0;
+        mach_timebase_info(&timebase_);
+    }
+
+    double nanoseconds () const
+    {
+        return static_cast<double>(elapsed_sec_) * 1e9 +
+            static_cast<double>(elapsed_nsec_);
+    }
+
+    double microseconds () const
+    {
+        return static_cast<double>(elapsed_sec_) * 1e6 +
+            static_cast<double>(elapsed_nsec_) / 1e3;
+    }
+
+    double milliseconds () const
+    {
+        return static_cast<double>(elapsed_sec_) * 1e3 +
+            static_cast<double>(elapsed_nsec_) / 1e6;
+    }
+
+    double seconds () const
+    {
+        return static_cast<double>(elapsed_sec_) +
+            static_cast<double>(elapsed_nsec_) / 1e9;
+    }
+
+    double minutes () const
+    {
+        return static_cast<double>(elapsed_sec_) / 60.0+
+            static_cast<double>(elapsed_nsec_) / 1e9 / 60.0;
+    }
+
+    double hours () const
+    {
+        return static_cast<double>(elapsed_sec_) / 3600.0+
+            static_cast<double>(elapsed_nsec_) / 1e9 / 3600.0;
+    }
+
+    private :
+
+    mutable uint64_t elapsed_sec_;
+    mutable uint64_t elapsed_nsec_;
+    mutable uint64_t start_time_;
+    mutable mach_timebase_info_data_t timebase_;
+    static const uint64_t ratio_ = 1000000000L; // 9 zero
 }; // class StopWatch
 
 } // namespace vsmc
@@ -130,9 +193,10 @@ class StopWatch
 
     void stop () const
     {
-        clock_gettime(CLOCK_REALTIME, &stop_time_);
-        time_t sec = stop_time_.tv_sec - start_time_.tv_sec;
-        long nsec = stop_time_.tv_nsec - start_time_.tv_nsec;
+        timespec stop_time;
+        clock_gettime(CLOCK_REALTIME, &stop_time);
+        time_t sec = stop_time.tv_sec - start_time_.tv_sec;
+        long nsec = stop_time.tv_nsec - start_time_.tv_nsec;
 
         elapsed_.tv_sec += sec;
         elapsed_.tv_nsec += nsec;
@@ -187,7 +251,6 @@ class StopWatch
 
     mutable timespec elapsed_;
     mutable timespec start_time_;
-    mutable timespec stop_time_;
     static const long ratio_ = 1000000000L; // 9 zero
 }; // class StopWatch
 
