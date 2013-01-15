fd9fd6ea3d9a2d9dbc299552b8f81dd249a94a4c
Yan Zhou <zhouyan@me.com>
2013-01-15 16:46:08 +0000

    implement linux clock

--- include/vsmc/utility/stop_watch.hpp
+++ include/vsmc/utility/stop_watch.hpp
@@ -7,6 +7,7 @@
 #include <vsmc/internal/forward.hpp>
 
 #if VSMC_HAS_CXX11LIB_CHRONO
+#define VSMC_STOP_WATCH_DEFINED
 
 #include <chrono>
 
@@ -81,6 +82,7 @@ class StopWatch
 } // namespace vsmc
 
 #elif defined(__APPLE__) || defined(__MACOSX)
+#define VSMC_STOP_WATCH_DEFINED
 
 namespace vsmc {
 
@@ -104,27 +106,11 @@ class StopWatch
 
 #elif defined(__linux__)
 
-namespace vsmc {
+#include <time.h>
+#include <features.h>
 
-class StopWatch
-{
-    public :
-
-    void start () const {}
-    void stop () const {}
-    void reset () const {}
-
-    double nanoseconds () const { return 0; }
-    double microseconds () const { return 0; }
-    double milliseconds () const { return 0; }
-    double seconds () const { return 0; }
-    double minutes () const { return 0; }
-    double hours () const { return 0; }
-}; // class StopWatch
-
-} // namespace vsmc
-
-#elif defined(__posix__)
+#if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 199309L
+#define VSMC_STOP_WATCH_DEFINED
 
 namespace vsmc {
 
@@ -132,42 +118,111 @@ class StopWatch
 {
     public :
 
-    void start () const {}
-    void stop () const {}
-    void reset () const {}
+    StopWatch ()
+    {
+        reset();
+    }
 
-    double nanoseconds () const { return 0; }
-    double microseconds () const { return 0; }
-    double milliseconds () const { return 0; }
-    double seconds () const { return 0; }
-    double minutes () const { return 0; }
-    double hours () const { return 0; }
+    void start () const
+    {
+        clock_gettime(CLOCK_REALTIME, &start_time_);
+    }
+
+    void stop () const
+    {
+        clock_gettime(CLOCK_REALTIME, &stop_time_);
+        time_t sec = stop_time_.tv_sec - start_time_.tv_sec;
+        long nsec = stop_time_.tv_nsec - start_time_.tv_nsec;
+
+        elapsed_.tv_sec += sec;
+        elapsed_.tv_nsec += nsec;
+        long inc_sec = elapsed_.tv_nsec / ratio_;
+        elapsed_.tv_sec += static_cast<time_t>(inc_sec);
+        elapsed_.tv_nsec -= inc_sec * ratio_;
+    }
+
+    void reset () const
+    {
+        elapsed_.tv_sec = 0;
+        elapsed_.tv_nsec = 0;
+    }
+
+    double nanoseconds () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) * 1e9 +
+            static_cast<double>(elapsed_.tv_nsec);
+    }
+
+    double microseconds () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) * 1e6 +
+            static_cast<double>(elapsed_.tv_nsec) / 1e3;
+    }
+
+    double milliseconds () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) * 1e3 +
+            static_cast<double>(elapsed_.tv_nsec) / 1e6;
+    }
+
+    double seconds () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) +
+            static_cast<double>(elapsed_.tv_nsec) / 1e9;
+    }
+
+    double minutes () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) / 60.0+
+            static_cast<double>(elapsed_.tv_nsec) / 1e9 / 60.0;
+    }
+
+    double hours () const
+    {
+        return static_cast<double>(elapsed_.tv_sec) / 3600.0+
+            static_cast<double>(elapsed_.tv_nsec) / 1e9 / 3600.0;
+    }
+
+    private :
+
+    mutable timespec elapsed_;
+    mutable timespec start_time_;
+    mutable timespec stop_time_;
+    static const long ratio_ = 1000000000L; // 9 zero
 }; // class StopWatch
 
 } // namespace vsmc
 
-#else // VSMC_HAS_CXX11LIB_CHRONO
-
-namespace vsmc {
-
-class StopWatch
-{
-    public :
-
-    void start () const {}
-    void stop () const {}
-    void reset () const {}
-
-    double nanoseconds () const { return 0; }
-    double microseconds () const { return 0; }
-    double milliseconds () const { return 0; }
-    double seconds () const { return 0; }
-    double minutes () const { return 0; }
-    double hours () const { return 0; }
-}; // class StopWatch
-
-} // namespace vsmc
+#endif // _POSIX_C_SOURCE
 
 #endif // VSMC_HAS_CXX11LIB_CHRONO
+
+#ifndef VSMC_STOP_WATCH_DEFINED
+
+namespace vsmc {
+
+class StopWatch
+{
+    public :
+
+    void start () const {}
+    void stop () const {}
+    void reset () const {}
+
+    double nanoseconds () const { return 0; }
+    double microseconds () const { return 0; }
+    double milliseconds () const { return 0; }
+    double seconds () const { return 0; }
+    double minutes () const { return 0; }
+    double hours () const { return 0; }
+}; // class StopWatch
+
+} // namespace vsmc
+
+#endif // VSMC_STOP_WATCH_DEFINED
+
+#ifdef VSMC_STOP_WATCH_DEFINED
+#undef VSMC_STOP_WATCH_DEFINED
+#endif
 
 #endif // VSMC_UTILITY_STOP_WATCH_HPP
