d8464f983f40bb5ea8bce8528f779b6b302b2249
Yan Zhou <zhouyan@me.com>
2013-01-31 15:31:12 +0000

    wrap ppl namespace inside vsmc

--- include/vsmc/utility/ppl_wrapper.hpp
+++ include/vsmc/utility/ppl_wrapper.hpp
@@ -3,10 +3,12 @@
 
 #include <ppl.h>
 
+namespace vsmc { 
 #if _MSC_VER >= 1700
 namespace ppl = ::concurrency;
 #else
 namespace ppl = ::Concurrency;
 #endif
+} // namespace vsmc
 
 #endif // VSMC_UTILITY_PPL_WRAPPER_HPP
