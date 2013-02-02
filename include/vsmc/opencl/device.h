ec06f9e231192b8ce3a1a3617becb023cb05c70c
Yan Zhou <zhouyan@me.com>
2013-02-02 23:20:03 +0000

    add device.h

---
+++ include/vsmc/opencl/device.h
@@ -0,0 +1,30 @@
+#ifndef VSMC_OPENCL_DEVICE_H
+#define VSMC_OPENCL_DEVICE_H
+
+#if defined(cl_khr_fp64)
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#elif defined(cl_amd_fp64)
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#endif
+
+#if defined(__OPENCL_C_VERSION__) && __OPENCL_C_VERSION__ >= 120
+#define VSMC_STATIC_INLINE static inline
+#else
+#define VSMC_STATIC_INLINE inline
+#endif
+
+__kernel
+void copy (__global state_type *state, __global size_type *copy_from)
+{
+    size_type to = get_global_id(0);
+
+    if (to >= Size)
+        return;
+
+    __global state_type *state_from = state + copy_from[to] * Dim;
+    __global state_type *state_to   = state + to * Dim;
+    for (uint i = 0; i != Dim; ++i)
+        state_to[i] = state_from[i];
+}
+
+#endif // VSMC_OPENCL_DEVICE_H
