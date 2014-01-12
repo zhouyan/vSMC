322bad7dd2f69a864b1fe694de45fcfb18536d95
Yan Zhou <zhouyan@me.com>
2014-01-12 05:18:57 +0000

    update VSMC_STATIC_INLINE def

--- include/vsmc/opencl/defines.h
+++ include/vsmc/opencl/defines.h
@@ -2,14 +2,24 @@
 #define VSMC_OPENCL_DEFINES_H
 
 #ifndef VSMC_STATIC_INLINE
-#if defined(__OPENCL_C_VERSION__) && __OPENCL_C_VERSION__ >= 120
-#define VSMC_STATIC_INLINE static inline
-#elif defined(__STDC__)
-#define VSMC_STATIC_INLINE static inline
+#ifdef __OPENCL_VERSION__
+  #if defined(__OPENCL_C_VERSION__) && __OPENCL_C_VERSION__ >= 120
+    #define VSMC_STATIC_INLINE static inline
+  #else
+    #define VSMC_STATIC_INLINE inline
+  #endif
 #else
-#define VSMC_STATIC_INLINE inline
+  #ifdef __cplusplus
+    #define VSMC_STATIC_INLINE inline
+  #else
+    #if defined(__STDC_VERSION__) && __STDC_VERSION__ > 199901L
+      #define VSMC_STATIC_INLINE static inline
+    #else
+      #error Define VSMC_STATIC_INLINE first
+    #endif
+  #endif
 #endif
-#endif
+#endif // VSMC_STATIC_INLINE
 
 #if defined(VSMC_FP_TYPE_IS_FLOAT) && defined(VSMC_FP_TYPE_IS_DOUBLE)
 #if VSMC_FP_TYPE_IS_FLOAT && VSMC_FP_TYPE_IS_DOUBLE
