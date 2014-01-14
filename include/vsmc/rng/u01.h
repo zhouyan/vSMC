0d216f6ef96c54e08068876b1750d13526f2b7d4
Yan Zhou <zhouyan@me.com>
2014-01-14 15:42:22 +0000

    cleanup

--- include/vsmc/rng/u01.h
+++ include/vsmc/rng/u01.h
@@ -57,98 +57,86 @@
 #define U01_CLOSED_CLOSED_64 u01_closed_closed_64_53
 #endif // VSMC_FP_TYPE_IS_DOUBLE
 
-#define VSMC_0x1p_31f (1.f/(1024.f*1024.f*1024.f*2.f))
-#define VSMC_0x1p_24f (128.f*VSMC_0x1p_31f)
-#define VSMC_0x1p_23f (256.f*VSMC_0x1p_31f)
-#define VSMC_0x1p_32  (1./(1024.*1024.*1024.*4.))
-#define VSMC_0x1p_63  (2.*VSMC_0x1p_32*VSMC_0x1p_32)
-#define VSMC_0x1p_53  (1024.*VSMC_0x1p_63)
-#define VSMC_0x1p_52  (2048.*VSMC_0x1p_63)
+#define VSMC_0x1p_31f (1.0f / (1024.0f * 1024.0f * 1024.0f * 2.0f))
+#define VSMC_0x1p_24f (128.0f * VSMC_0x1p_31f)
+#define VSMC_0x1p_23f (256.0f * VSMC_0x1p_31f)
+#define VSMC_0x1p_32  (1.0 / (1024.0 *1024.0 * 1024.0 * 4.0))
+#define VSMC_0x1p_63  (2.0 * VSMC_0x1p_32 * VSMC_0x1p_32)
+#define VSMC_0x1p_53  (1024.0 * VSMC_0x1p_63)
+#define VSMC_0x1p_52  (2048.0 * VSMC_0x1p_63)
 
-/* narrowing conversions:  uint32_t to float */
 /// \ingroup RNG
 VSMC_STATIC_INLINE float u01_closed_closed_32_24 (uint32_t i)
 {
-    /* N.B.  we ignore the high bit, so output is not monotonic */
-    return ((i&0x7fffffc0) + (i&0x40))*VSMC_0x1p_31f; /* 0x1.p-31f */
+    return ((i&0x7fffffc0) + (i&0x40)) * VSMC_0x1p_31f;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE float u01_closed_open_32_24 (uint32_t i)
 {
-    return (i>>8)*VSMC_0x1p_24f; /* 0x1.0p-24f; */
+    return (i>>8) * VSMC_0x1p_24f;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE float u01_open_closed_32_24 (uint32_t i)
 {
-    return (1+(i>>8))*VSMC_0x1p_24f; /* *0x1.0p-24f; */
+    return (1 + (i>>8)) * VSMC_0x1p_24f;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE float u01_open_open_32_24 (uint32_t i)
 {
-    return (0.5f+(i>>9))*VSMC_0x1p_23f; /* 0x1.p-23f; */
+    return (0.5f + (i>>9)) * VSMC_0x1p_23f;
 }
 
 #if VSMC_FP_TYPE_IS_DOUBLE
 
-/* narrowing conversions:  uint64_t to double */
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_closed_closed_64_53 (uint64_t i)
 {
-    /* N.B.  we ignore the high bit, so output is not monotonic */
-    return ((i&R123_64BIT(0x7ffffffffffffe00)) + (i&0x200))*VSMC_0x1p_63; /* 0x1.p-63; */
+    return ((i&R123_64BIT(0x7ffffffffffffe00)) + (i&0x200)) * VSMC_0x1p_63;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_closed_open_64_53 (uint64_t i)
 {
-    return (i>>11)*VSMC_0x1p_53; /* 0x1.0p-53; */
+    return (i>>11) * VSMC_0x1p_53;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_open_closed_64_53 (uint64_t i)
 {
-    return (1+(i>>11))*VSMC_0x1p_53; /* 0x1.0p-53; */
+    return (1 + (i>>11)) * VSMC_0x1p_53;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_open_open_64_53 (uint64_t i)
 {
-    return (0.5+(i>>12))*VSMC_0x1p_52; /* 0x1.0p-52; */
+    return (0.5 + (i>>12)) * VSMC_0x1p_52;
 }
 
-/* widening conversions:  u32 to double */
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_closed_closed_32_53 (uint32_t i)
 {
-    /* j = i+(i&1) takes on 2^31+1 possible values with a 'trapezoid'
-     * distribution:
-     * p_j =  1 0 2 0 2 .... 2 0 2 0 1
-     * j   =  0 1 2 3 4 ....        2^32
-     * by converting to double *before* doing the add, we don't wrap the high
-     * bit.
-     */
-    return (((double)(i&1)) + i)*VSMC_0x1p_32; /* 0x1.p-32; */
+    return (((double)(i&1)) + i) * VSMC_0x1p_32;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_closed_open_32_53 (uint32_t i)
 {
-    return i*VSMC_0x1p_32; /* 0x1.p-32; */
+    return i * VSMC_0x1p_32;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_open_closed_32_53 (uint32_t i)
 {
-    return (1.+i)*VSMC_0x1p_32; /* 0x1.p-32; */
+    return (1.0 + i) * VSMC_0x1p_32;
 }
 
 /// \ingroup RNG
 VSMC_STATIC_INLINE double u01_open_open_32_53 (uint32_t i)
 {
-    return (0.5+i)*VSMC_0x1p_32; /* 0x1.p-32; */
+    return (0.5 + i) * VSMC_0x1p_32;
 }
 
 #endif // VSMC_FP_TYPE_IS_DOUBLE
