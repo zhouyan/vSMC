c76e6e2632b1204e9cb40af4bfa9d7f99510f67c
Yan Zhou <zhouyan@me.com>
2014-01-28 01:52:55 +0000

    fix rng_set_tbb

--- include/vsmc/rng/rng_set_tbb.hpp
+++ include/vsmc/rng/rng_set_tbb.hpp
@@ -16,25 +16,35 @@ class RngSetTBB
     typedef RngType rng_type;
     typedef std::size_t size_type;
 
-    explicit RngSetTBB (size_type N) : size_(N) {init_rng();}
+    explicit RngSetTBB (size_type N) : size_(N), flag_(flag_init) {}
 
     size_type size () const {return size_;}
 
-    rng_type &rng (size_type id = 0) {return rng_.local();}
+    rng_type &rng (size_type id = 0)
+    {
+        init_rng();
+        return rng_.local();
+    }
 
     private :
 
     std::size_t size_;
     traits::RngShift<rng_type> shift_;
-    tbb::combinable<rng_type> rng_;
     tbb::mutex mtx_;
+    tbb::combinable<rng_type> rng_;
+    tbb::combinable<bool> flag_;
 
     void init_rng ()
     {
+        if (flag_.local()) return;
+
         tbb::mutex::scoped_lock lock(mtx_);
         rng_.local().seed(Seed::instance().get());
         shift_(rng_.local());
+        flag_.local() = true;
     }
+
+    static bool flag_init () {return false;}
 }; // class RngSetTBB
 
 } // namespace vsmc
