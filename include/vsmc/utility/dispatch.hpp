55b3c139000b3b4f9d743e8c27f2e839981f5622
Yan Zhou <zhouyan@me.com>
2014-02-09 12:45:28 +0000

    rework on DispatchFunction

--- include/vsmc/utility/dispatch.hpp
+++ include/vsmc/utility/dispatch.hpp
@@ -35,12 +35,19 @@ template <DispatchSourceType> class DispatchSource;
 
 /// \brief Wrap a callable object into a `dispatch_function_t` type pointer
 /// \ingroup Dispatch
+///
+/// \details
+/// It is important that this object live at least after the completion of the
+/// work. This is a thin wrapper around a regular C++ callable object
+/// (including lambda expressions) that make it easier to interface them with
+/// GCD. The memory management issue is the same as using raw GCD interface
+/// with a context variable, which shall not be destroyed before the execution
+/// of the function finishes. The original object does not need to exist after
+/// the creation of DispatchFunction.
 template <typename T>
 class DispatchFunction
 {
     public :
-
-    typedef dispatch_function_t function_type;
 
     DispatchFunction (const T &work) : work_(work) {}
 
@@ -70,103 +77,39 @@ class DispatchFunction
     }
 #endif
 
-    void *context () const {return (void *) &work_;}
+    void *context () {return (void *) this;}
 
-    function_type function () const {return function_;}
+    dispatch_function_t function () const {return function_;}
 
     private :
 
     T work_;
 
-    static void function_ (void *work) {(*static_cast<T *>(work))();}
+    static void function_ (void *ctx)
+    {
+        DispatchFunction<T> *df_ptr = static_cast<DispatchFunction<T> *>(ctx);
+        df_ptr->work_();
+    }
 }; // class DispatchFunction
 
-/// \brief Wrap a callable object into a `void (*) (void *, std::size_t)` type
-/// pointer
+#if VSMC_HAS_CXX11_RVALUE_REFERENCES
+/// \brief Make a DispatchFunction object from an arbitrary callable object
 /// \ingroup Dispatch
+///
+/// \param work A callable object with signature `void f(void)`
 template <typename T>
-class DispatchFunctionApply
+DispatchFunction<
+typename cxx11::remove_cv<typename cxx11::remove_reference<T>::type>::type>
+*dispatch_function_new (T &&work) VSMC_NOEXCEPT
 {
-    public :
-
-    typedef void (*function_type) (void *, std::size_t);
-
-    DispatchFunctionApply (const T &work) : work_(work) {}
-
-    DispatchFunctionApply (const DispatchFunctionApply<T> &other) :
-        work_(other.work_) {}
-
-    DispatchFunctionApply<T> &operator= (const DispatchFunctionApply<T> &other)
-    {
-        if (this != &other)
-            work_ = other.work_;
-
-        return *this;
-    }
-
-#if VSMC_HAS_CXX11_RVALUE_REFERENCES
-    DispatchFunctionApply (T &&work) VSMC_NOEXCEPT :
-        work_(cxx11::move(work)) {}
-
-    DispatchFunctionApply (DispatchFunctionApply<T> &&other) VSMC_NOEXCEPT :
-        work_(cxx11::move(other.work_)) {}
-
-    DispatchFunctionApply<T> &operator= (
-            DispatchFunctionApply<T> &&other) VSMC_NOEXCEPT
-    {
-        if (this != &other)
-            work_ = cxx11::move(other.work_);
-
-        return *this;
-    }
-#endif
-
-    void *context () const {return (void *) &work_;}
-
-    function_type function () const {return function_;}
-
-    private :
-
-    T work_;
-
-    static void function_ (void *work, std::size_t i)
-    {(*static_cast<T *>(work))(i);}
-}; // class DispatchFunctionApply
-
-/// \brief Make a DispatchFunction object from an arbitrary callable object
-/// \ingroup Dispatch
-///
-/// \param work A callable object with signature `void f(void)`
+    typedef typename cxx11::remove_reference<T>::type U;
+    typedef typename cxx11::remove_cv<U>::type V;
+    return new DispatchFunction<V>(cxx11::forward<T>(work));
+}
+#else
 template <typename T>
-DispatchFunction<T> dispatch_make_function (T &work)
-{return DispatchFunction<T>(work);}
-
-/// \brief Make a DispatchFunctionApply object from an arbitrary callable
-/// object
-/// \ingroup Dispatch
-///
-/// \param work A callable object with signature `void f(std::size_t)`
-template <typename T>
-DispatchFunctionApply<T> dispatch_make_function_apply (T &work)
-{return DispatchFunctionApply<T>(work);}
-
-#if VSMC_HAS_CXX11_RVALUE_REFERENCES
-/// \brief Make a DispatchFunction object from an arbitrary callable object
-/// \ingroup Dispatch
-///
-/// \param work A callable object with signature `void f(void)`
-template <typename T>
-DispatchFunction<T> dispatch_make_function (T &&work) VSMC_NOEXCEPT
-{return DispatchFunction<T>(cxx11::move(work));}
-
-/// \brief Make a DispatchFunctionApply object from an arbitrary callable
-/// object
-/// \ingroup Dispatch
-///
-/// \param work A callable object with signature `void f(std::size_t)`
-template <typename T>
-DispatchFunctionApply<T> dispatch_make_function_apply (T &&work) VSMC_NOEXCEPT
-{return DispatchFunctionApply<T>(cxx11::move(work));}
+DispatchFunction<T> *dispatch_function_new (const T &work)
+{return new DispatchFunction<T>(work);}
 #endif
 
 /// \brief Base class of Dispatch objects
@@ -303,7 +246,12 @@ class DispatchQueueBase : public DispatchObject<dispatch_queue_t>
         DispatchObject<dispatch_queue_t>(other) {}
 
     DispatchQueueBase &operator= (const DispatchQueueBase &other)
-    {DispatchObject<dispatch_queue_t>::operator=(other); return *this;}
+    {
+        if (this != &other)
+            DispatchObject<dispatch_queue_t>::operator=(other);
+
+        return *this;
+    }
 }; // class DispatchQueueBase
 
 /// \brief The main dispatch queue (`dipatch_get_main_queue`)
