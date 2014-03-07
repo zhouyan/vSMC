7615cea7ff548c75996b49ef487dcc26c3770d9f
Yan Zhou <zhouyan@me.com>
2014-03-07 07:00:02 +0000

    fix aesni

--- include/vsmc/rng/aesni.hpp
+++ include/vsmc/rng/aesni.hpp
@@ -11,13 +11,31 @@
              cxx11::is_same<ResultType, __m128i>::value),                    \
             USE_AESNIEngine_WITH_INTEGER_TYPE_OTHER_THAN_uint32_t_OR_uint64_t_OR_m128i)
 
-#define VSMC_STATIC_ASSERT_RNG_AESNI_ROUND_0(R) \
-    VSMC_STATIC_ASSERT((R > 0), USE_AESNIEngine_WITH_ZERO_ROUND)
-
 #define VSMC_STATIC_ASSERT_RNG_AESNI \
-    VSMC_STATIC_ASSERT_RNG_AESNI_ROUND_0(R);
+    VSMC_STATIC_ASSERT_RNG_AESNI_RESULT_TYPE(ResultType);
+
+#define VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(N, val) \
+    template <> struct AESNIRoundConstant< N > :                             \
+        public cxx11::integral_constant<int, val > {};
 
 namespace vsmc {
+
+namespace internal {
+
+template <std::size_t N> struct AESNIRoundConstant;
+
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(0, 0x01)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(1, 0x02)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(2, 0x04)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(3, 0x08)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(4, 0x10)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(5, 0x20)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(6, 0x40)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(7, 0x80)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(8, 0x1B)
+VSMC_DEFINE_RNG_AESNI_ROUND_CONSTANT(9, 0x36)
+
+} // namespace vsmc::internal
 
 /// \brief AESNI RNG engine reimplemented
 /// \ingroup R123RNG
@@ -31,9 +49,10 @@ namespace vsmc {
 /// does not have to be 10. Otherwise it is almost identical. However, unlike
 /// the reimplementation ThreefryEngine and PhiloxEngine, this engine won't
 /// give exactly the same output as `AESNI4x32_R` etc.
-template <typename ResultType, std::size_t R = 10>
+template <typename ResultType>
 class AESNIEngine
 {
+    static VSMC_CONSTEXPR const std::size_t R_ = 10;
     static VSMC_CONSTEXPR const std::size_t K_ =
         sizeof(__m128i) / sizeof(ResultType);
 
@@ -41,7 +60,7 @@ class AESNIEngine
 
     typedef ResultType result_type;
     typedef StaticVector<ResultType, K_> ctr_type;
-    typedef StaticVector<__m128i, R + 1> key_type;
+    typedef StaticVector<__m128i, R_ + 1> key_type;
 
     explicit AESNIEngine (result_type s = 0) :
         pac_(_mm_setzero_si128()),
@@ -127,8 +146,8 @@ class AESNIEngine
     static VSMC_CONSTEXPR result_type max VSMC_MNE () {return _Max;}
 
     friend inline bool operator== (
-            const AESNIEngine<ResultType, R> &eng1,
-            const AESNIEngine<ResultType, R> &eng2)
+            const AESNIEngine<ResultType> &eng1,
+            const AESNIEngine<ResultType> &eng2)
     {
         return
             eng1.ctr_ == eng2.ctr_ &&
@@ -137,8 +156,8 @@ class AESNIEngine
     }
 
     friend inline bool operator!= (
-            const AESNIEngine<ResultType, R> &eng1,
-            const AESNIEngine<ResultType, R> &eng2)
+            const AESNIEngine<ResultType> &eng1,
+            const AESNIEngine<ResultType> &eng2)
     {return !(eng1 == eng2);}
 
     private :
@@ -163,13 +182,13 @@ class AESNIEngine
 
     template <std::size_t>
     void generate (cxx11::false_type)
-    {pac_ = _mm_aesenclast_si128(pac_, key_[R]);}
+    {pac_ = _mm_aesenclast_si128(pac_, key_[R_]);}
 
     template <std::size_t N>
     void generate (cxx11::true_type)
     {
         pac_ = _mm_aesenc_si128(pac_, key_[N + 1]);
-        generate<N + 1>(cxx11::integral_constant<bool, N + 1 < R>());
+        generate<N + 1>(cxx11::integral_constant<bool, N + 1 < R_>());
     }
 
     void init_key(__m128i k)
@@ -185,25 +204,26 @@ class AESNIEngine
     {return _mm_set1_epi64x(static_cast<int64_t>(s));}
 
     template <std::size_t>
-    void init_key (cxx11::false_type) {key_[R] = tmp0_;}
+    void init_key (cxx11::false_type) {key_[R_] = tmp0_;}
 
     template <std::size_t N>
     void init_key (cxx11::true_type)
     {
         key_[N] = tmp0_;
-        tmp1_ = _mm_aeskeygenassist_si128(tmp0_, 1<<N);
+        tmp1_ = _mm_aeskeygenassist_si128(tmp0_,
+                internal::AESNIRoundConstant<N>::value);
         init_key_assit();
-        init_key<N + 1>(cxx11::integral_constant<bool, N < R>());
+        init_key<N + 1>(cxx11::integral_constant<bool, N + 1 < R_>());
     }
 
     void init_key_assit ()
     {
         tmp1_ = _mm_shuffle_epi32 (tmp1_ ,0xFF);
-        tmp2_ = _mm_slli_si128    (tmp0_, 0x4);
+        tmp2_ = _mm_slli_si128    (tmp0_, 0x04);
         tmp0_ = _mm_xor_si128     (tmp0_, tmp2_);
-        tmp2_ = _mm_slli_si128    (tmp2_, 0x4);
+        tmp2_ = _mm_slli_si128    (tmp2_, 0x04);
         tmp0_ = _mm_xor_si128     (tmp0_, tmp2_);
-        tmp2_ = _mm_slli_si128    (tmp2_, 0x4);
+        tmp2_ = _mm_slli_si128    (tmp2_, 0x04);
         tmp0_ = _mm_xor_si128     (tmp0_, tmp2_);
         tmp0_ = _mm_xor_si128     (tmp0_, tmp1_);
     }
