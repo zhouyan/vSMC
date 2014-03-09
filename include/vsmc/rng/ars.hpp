0fefaa7c6591961786862c6440ed56621429ea3d
Yan Zhou <zhouyan@me.com>
2014-03-09 21:50:44 +0000

    ARS can have multiple blocks now

--- include/vsmc/rng/ars.hpp
+++ include/vsmc/rng/ars.hpp
@@ -15,9 +15,21 @@
 #define VSMC_STATIC_ASSERT_RNG_ARS_ROUND(R) \
     VSMC_STATIC_ASSERT((R <= 10), USE_ARSEngine_WITH_ROUNDS_LARGER_THAN_10)
 
+#define VSMC_STATIC_ASSERT_RNG_ARS_BLOCKS(Blocks) \
+    VSMC_STATIC_ASSERT((Blocks > 0), USE_ARSEngine_WITH_ZERO_BLOCKS)
+
 #define VSMC_STATIC_ASSERT_RNG_ARS \
     VSMC_STATIC_ASSERT_RNG_ARS_ROUND_0(R);                                   \
-    VSMC_STATIC_ASSERT_RNG_ARS_ROUND(R);
+    VSMC_STATIC_ASSERT_RNG_ARS_ROUND(R);                                     \
+    VSMC_STATIC_ASSERT_RNG_ARS_BLOCKS(Blocks);
+
+#ifndef VSMC_RNG_ARS_BLOCKS
+#define VSMC_RNG_ARS_BLOCKS 1
+#endif
+
+#ifndef VSMC_RNG_ARS_ROUNDS
+#define VSMC_RNG_ARS_ROUNDS 10
+#endif
 
 namespace vsmc {
 
@@ -64,7 +76,9 @@ template <std::size_t N> struct ARSConstantTrait :
 /// This implementation is slightly more flexible than the original. First it
 /// allows using 64-bits integers as output. Second It allows setting the Weyl
 /// constants through trait `vsmc::traits::ARSConstantTrait`.
-template <typename ResultType, std::size_t R = 10>
+template <typename ResultType,
+         std::size_t Blocks = VSMC_RNG_ARS_BLOCKS,
+         std::size_t R = VSMC_RNG_ARS_ROUNDS>
 class ARSEngine
 {
     static VSMC_CONSTEXPR const std::size_t K_ =
@@ -80,7 +94,7 @@ class ARSEngine
         par_(_mm_setzero_si128()), weyl_ (_mm_set_epi64x(
                     static_cast<int64_t>(traits::ARSConstantTrait<0>::value),
                     static_cast<int64_t>(traits::ARSConstantTrait<1>::value))),
-        pac_(par_), remain_(0)
+        remain_(0)
     {
         VSMC_STATIC_ASSERT_RNG_ARS;
         seed(s);
@@ -93,7 +107,7 @@ class ARSEngine
         par_(_mm_setzero_si128()), weyl_ (_mm_set_epi64x(
                     static_cast<int64_t>(traits::ARSConstantTrait<0>::value),
                     static_cast<int64_t>(traits::ARSConstantTrait<1>::value))),
-        pac_(par_), remain_(0)
+        remain_(0)
     {
         VSMC_STATIC_ASSERT_RNG_ARS;
         seed(seq);
@@ -101,7 +115,9 @@ class ARSEngine
 
     void seed (result_type s)
     {
-        ctr_.fill(0);
+        ctr_type c;
+        c.fill(0);
+        ctr_.fill(c);
         key_.fill(0);
         key_.front() = s;
         remain_ = 0;
@@ -112,16 +128,25 @@ class ARSEngine
             !internal::is_seed_sequence<SeedSeq, ResultType>::value>::type * =
             VSMC_NULLPTR)
     {
-        ctr_.fill(0);
+        ctr_type c;
+        c.fill(0);
+        ctr_.fill(c);
         seq.generate(key_.begin(), key_.end());
         remain_ = 0;
     }
 
-    const ctr_type &ctr () const {return ctr_;}
+    const StaticVector<ctr_type, Blocks> &ctr () const {return ctr_;}
 
     const key_type &key () const {return key_;}
 
     void ctr (const ctr_type &c)
+    {
+        ctr_.back() = c;
+        increment();
+        remain_ = 0;
+    }
+
+    void ctr (const StaticVector<ctr_type, Blocks> &c)
     {
         ctr_ = c;
         remain_ = 0;
@@ -133,45 +158,36 @@ class ARSEngine
         remain_ = 0;
     }
 
-    /// \brief Same as operator() but return the __m128i type
-    __m128i generate ()
-    {
-        internal::RngCounter<ResultType, K_>::increment(ctr_);
-        pack();
-        generate<0>(cxx11::integral_constant<bool, 1 < R>());
-        remain_ = 0;
-
-        return pac_;
-    }
-
     result_type operator() ()
     {
-        if (remain_ == 0) {
+        if (remain_ == 0)
             generate();
-            internal::unpack(pac_, res_);
-            remain_ = K_;
-        }
+        --remain_;
 
-        return res_[--remain_];
+        return result<Blocks - 1>(
+                cxx11::integral_constant<bool, 1 < Blocks>());
     }
 
+    /// \brief Discard results
+    ///
+    /// \details
+    /// The the behavior is slightly different from that in C++11 standard.
+    /// Calling `discard(nskip)` is not equivalent to call `operator()` `nskip`
+    /// times. Instead, it ensures that at least `nskip` results are discarded.
+    /// There may be a few more than `nskip` also discarded.
     void discard (std::size_t nskip)
     {
         if (nskip == 0)
             return;
 
-        --nskip;
-        internal::RngCounter<ResultType, K_>::increment(ctr_, nskip);
         remain_ = 0;
-        operator()();
-        nskip = nskip % K_;
-        if (remain_ >= nskip) {
-            remain_ -= nskip;
+        increment();
+        if (nskip <= Blocks)
             return;
-        }
 
-        nskip -= remain_;
-        remain_ = K_ - nskip;
+        nskip -= Blocks;
+        internal::RngCounter<ResultType, K_>::increment(ctr_.front(), nskip);
+        increment();
     }
 
     static VSMC_CONSTEXPR const result_type _Min = 0;
@@ -182,8 +198,8 @@ class ARSEngine
     static VSMC_CONSTEXPR result_type max VSMC_MNE () {return _Max;}
 
     friend inline bool operator== (
-            const ARSEngine<ResultType> &eng1,
-            const ARSEngine<ResultType> &eng2)
+            const ARSEngine<ResultType, Blocks, R> &eng1,
+            const ARSEngine<ResultType, Blocks, R> &eng2)
     {
         if (eng1.ctr_ != eng2.ctr_)
             return false;
@@ -198,21 +214,22 @@ class ARSEngine
     }
 
     friend inline bool operator!= (
-            const ARSEngine<ResultType> &eng1,
-            const ARSEngine<ResultType> &eng2)
+            const ARSEngine<ResultType, Blocks, R> &eng1,
+            const ARSEngine<ResultType, Blocks, R> &eng2)
     {return !(eng1 == eng2);}
 
     template <typename CharT, typename Traits>
     friend inline std::basic_ostream<CharT, Traits> &operator<< (
             std::basic_ostream<CharT, Traits> &os,
-            const ARSEngine<ResultType> &eng)
+            const ARSEngine<ResultType, Blocks, R> &eng)
     {
         if (os) os << eng.ctr_ << ' ';
         if (os) os << eng.res_ << ' ';
         if (os) os << eng.key_ << ' ';
         internal::output_m128i(os, eng.par_);  if (os) os << ' ';
         internal::output_m128i(os, eng.weyl_); if (os) os << ' ';
-        internal::output_m128i(os, eng.pac_);  if (os) os << ' ';
+        for (std::size_t i = 0; i != Blocks; ++i)
+            internal::output_m128i(os, eng.pac_[i]);  if (os) os << ' ';
         if (os) os << eng.remain_;
 
         return os;
@@ -221,7 +238,7 @@ class ARSEngine
     template <typename CharT, typename Traits>
     friend inline std::basic_istream<CharT, Traits> &operator>> (
             std::basic_istream<CharT, Traits> &is,
-            ARSEngine<ResultType> &eng)
+            ARSEngine<ResultType, Blocks, R> &eng)
     {
         ARSEngine eng_tmp;
         if (is) is >> std::ws >> eng_tmp.ctr_;
@@ -229,7 +246,8 @@ class ARSEngine
         if (is) is >> std::ws >> eng_tmp.key_;
         internal::input_m128i(is, eng_tmp.par_);
         internal::input_m128i(is, eng_tmp.weyl_);
-        internal::input_m128i(is, eng_tmp.pac_);
+        for (std::size_t i = 0; i != Blocks; ++i)
+            internal::input_m128i(is, eng_tmp.pac_[i]);
         if (is) is >> std::ws >> eng_tmp.remain_;
         if (is) eng = eng_tmp;
 
@@ -238,48 +256,205 @@ class ARSEngine
 
     private :
 
-    ctr_type ctr_;
-    ctr_type res_;
+    StaticVector<ctr_type, Blocks> ctr_;
+    StaticVector<ctr_type, Blocks> res_;
     key_type key_;
     __m128i par_;
     __m128i weyl_;
-    __m128i pac_;
+    StaticVector<__m128i, Blocks> pac_;
     std::size_t remain_;
+
+    void increment ()
+    {
+        ctr_.front() = ctr_.back();
+        internal::RngCounter<ResultType, K_>::increment(ctr_.front());
+        increment<1>(cxx11::integral_constant<bool, 1 < Blocks>());
+    }
+
+    template <std::size_t> void increment (cxx11::false_type) {}
+
+    template <std::size_t B>
+    void increment (cxx11::true_type)
+    {
+        ctr_[Position<B>()] = ctr_[Position<B - 1>()];
+        internal::RngCounter<ResultType, K_>::increment(ctr_[Position<B>()]);
+        increment<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
+    }
 
     void pack ()
     {
-        internal::pack(ctr_, pac_);
         internal::pack(key_, par_);
-        pac_ = _mm_xor_si128(pac_, par_);
+        internal::pack(ctr_.front(), pac_.front());
+        pack<1>(cxx11::integral_constant<bool, 1 < Blocks>());
+        pac_.front() = _mm_xor_si128(pac_.front(), par_);
+        pac_xor<1>(cxx11::integral_constant<bool, 1 < Blocks>());
+    }
+
+    template <std::size_t> void pack (cxx11::false_type) {}
+
+    template <std::size_t B>
+    void pack (cxx11::true_type)
+    {
+        internal::pack(ctr_[Position<B>()], pac_[Position<B>()]);
+        pack<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
+    }
+
+    template <std::size_t> void pac_xor (cxx11::false_type) {}
+
+    template <std::size_t B>
+    void pac_xor (cxx11::true_type)
+    {
+        pac_[Position<B>()] = _mm_xor_si128(pac_[Position<B>()], par_);
+        pac_xor<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
+    }
+
+    template <std::size_t> void unpack (cxx11::false_type) {}
+
+    void unpack ()
+    {
+        internal::unpack(pac_.front(), res_.front());
+        unpack<1>(cxx11::integral_constant<bool, 1 < Blocks>());
+    }
+
+    template <std::size_t B>
+    void unpack (cxx11::true_type)
+    {
+        internal::unpack(pac_[Position<B>()], res_[Position<B>()]);
+        unpack<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
+    }
+
+    void generate ()
+    {
+        increment();
+        pack();
+        generate<0>(cxx11::true_type());
+        unpack();
+        remain_ = K_ * Blocks;
     }
 
     template <std::size_t>
     void generate (cxx11::false_type)
     {
         par_ = _mm_add_epi64(par_, weyl_);
-        pac_ = _mm_aesenclast_si128(pac_, par_);
+        pac_.front() = _mm_aesenclast_si128(pac_.front(), par_);
+        generate_last<1>(cxx11::integral_constant<bool, 1 < Blocks>());
+    }
+
+    template <std::size_t> void generate_last (cxx11::false_type) {}
+
+    template <std::size_t B>
+    void generate_last (cxx11::true_type)
+    {
+        pac_[Position<B>()] = _mm_aesenclast_si128(pac_[Position<B>()], par_);
+        generate_last<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
     }
 
     template <std::size_t N>
     void generate (cxx11::true_type)
     {
         par_ = _mm_add_epi64(par_, weyl_);
-        pac_ = _mm_aesenc_si128(pac_, par_);
+        pac_.front() = _mm_aesenc_si128(pac_.front(), par_);
+        generate_step<1>(cxx11::integral_constant<bool, 1 < Blocks>());
         generate<N + 1>(cxx11::integral_constant<bool, N  + 2 < R>());
+    }
+
+    template <std::size_t>
+    void generate_step (cxx11::false_type) {}
+
+    template <std::size_t B>
+    void generate_step (cxx11::true_type)
+    {
+        pac_[Position<B>()] = _mm_aesenc_si128(pac_[Position<B>()], par_);
+        generate_step<B + 1>(cxx11::integral_constant<bool, B + 1 < Blocks>());
+    }
+
+    template <std::size_t>
+    result_type result (cxx11::false_type)
+    {return res_.front()[remain_];}
+
+    template <std::size_t I>
+    result_type result (cxx11::true_type)
+    {
+        if (remain_ > I * K_)
+            return res_[Position<I>()][remain_ - I * K_];
+        return result<I - 1>(cxx11::integral_constant<bool, 0 < I>());
     }
 }; // class ARSEngine
 
-/// \brief ARS RNG engine returning 32-bits integers
+/// \brief ARS RNG engine returning 32-bits integers with default blocks and
+/// default rounds
 /// \ingroup R123RNG
 typedef ARSEngine<uint32_t> ARS4x32;
 
-/// \brief ARS RNG engine returning 64-bits integers
+/// \brief ARS RNG engine returning 64-bits integers with default blocks and
+/// default rounds
 /// \ingroup R123RNG
 typedef ARSEngine<uint64_t> ARS2x64;
 
-/// \brief ARS RNG engine returning 128-bits integers
+/// \brief ARS RNG engine returning 128-bits integers with default blocks and
+/// default rounds
 /// \ingroup R123RNG
 typedef ARSEngine<__m128i>  ARS1x128;
+
+/// \brief ARS RNG engine returning 32-bits integers with 1 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint32_t, 1> ARS4x32_1;
+
+/// \brief ARS RNG engine returning 64-bits integers with 1 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint64_t, 1> ARS2x64_1;
+
+/// \brief ARS RNG engine returning 128-bits integers with 1 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<__m128i, 1>  ARS1x128_1;
+
+/// \brief ARS RNG engine returning 32-bits integers with 2 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint32_t, 2> ARS4x32_2;
+
+/// \brief ARS RNG engine returning 64-bits integers with 2 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint64_t, 2> ARS2x64_2;
+
+/// \brief ARS RNG engine returning 128-bits integers with 2 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<__m128i, 2>  ARS1x128_2;
+
+/// \brief ARS RNG engine returning 32-bits integers with 4 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint32_t, 4> ARS4x32_4;
+
+/// \brief ARS RNG engine returning 64-bits integers with 4 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint64_t, 4> ARS2x64_4;
+
+/// \brief ARS RNG engine returning 128-bits integers with 4 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<__m128i, 4>  ARS1x128_4;
+
+/// \brief ARS RNG engine returning 32-bits integers with 8 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint32_t, 8> ARS4x32_8;
+
+/// \brief ARS RNG engine returning 64-bits integers with 8 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<uint64_t, 8> ARS2x64_8;
+
+/// \brief ARS RNG engine returning 128-bits integers with 8 block and default
+/// rounds
+/// \ingroup R123RNG
+typedef ARSEngine<__m128i, 8>  ARS1x128_8;
 
 } // namespace vsmc
 
