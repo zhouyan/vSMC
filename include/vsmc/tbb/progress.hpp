544bd30a4fb2774dc5857f1e41da11985ea0e952
Yan Zhou <zhouyan@me.com>
2014-06-15 14:21:57 +0800

    new header place holders

--- include/vsmc/tbb/progress.hpp
+++ include/vsmc/tbb/progress.hpp
@@ -4,17 +4,4 @@
 #include <vsmc/internal/common.hpp>
 #include <tbb/tbb.h>
 
-namespace vsmc {
-
-/// \brief Progress bar using Intel TBB
-/// \ingroup TBBUtility
-class TBBProgress
-{
-    public :
-
-    private :
-}; // class TBBProgress
-
-} // namespace vsmc
-
 #endif // VSMC_TBB_PROGRESS_HPP
---
+++ include/vsmc/tbb/timer.hpp
@@ -0,0 +1,7 @@
+#ifndef VSMC_TBB_TIMER_HPP
+#define VSMC_TBB_TIMER_HPP
+
+#include <vsmc/internal/common.hpp>
+#include <tbb/tbb.h>
+
+#endif // VSMC_TBB_TIMER_HPP
