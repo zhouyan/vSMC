d49e229c5c2c2926a0febb41a56b74f3537f93ef
Yan Zhou <zhouyan@me.com>
2014-06-16 02:18:19 +0800

    remove empty headers

--- include/vsmc/tbb/progress.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef VSMC_TBB_PROGRESS_HPP
-#define VSMC_TBB_PROGRESS_HPP
-
-#include <vsmc/internal/common.hpp>
-#include <tbb/tbb.h>
-
-#endif // VSMC_TBB_PROGRESS_HPP
--- include/vsmc/tbb/timer.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef VSMC_TBB_TIMER_HPP
-#define VSMC_TBB_TIMER_HPP
-
-#include <vsmc/internal/common.hpp>
-#include <tbb/tbb.h>
-
-#endif // VSMC_TBB_TIMER_HPP
