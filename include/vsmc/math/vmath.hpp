5d0ffb97ecc00649f7e5bc16abaa255862ad6b8f
Yan Zhou <zhouyan@me.com>
2014-10-14 05:09:40 +0800

    simply dynamic dispatch

--- include/vsmc/math/vmath.hpp
+++ include/vsmc/math/vmath.hpp
@@ -14,30 +14,30 @@
 #include <vsmc/cxx11/cmath.hpp>
 
 #define VSMC_DEFINE_VMATH_1(ns, name) \
-    template <typename T>                                                    \
-    inline void v##name (std::size_t n, const T *a, T *y)                    \
-    {                                                                        \
-        using ns::name;                                                      \
-        for (std::size_t i = 0; i != n; ++i)                                 \
-            y[i] = name(a[i]);                                               \
-    }
+template <typename T>                                                        \
+inline void v##name (std::size_t n, const T *a, T *y)                        \
+{                                                                            \
+    using ns::name;                                                          \
+    for (std::size_t i = 0; i != n; ++i)                                     \
+        y[i] = name(a[i]);                                                   \
+}
 
 #define VSMC_DEFINE_VMATH_2(ns, name) \
-    template <typename T>                                                    \
-    inline void v##name (std::size_t n, const T *a, const T *b, T *y)        \
-    {                                                                        \
-        using ns::name;                                                      \
-        for (std::size_t i = 0; i != n; ++i)                                 \
-            y[i] = name(a[i], b[i]);                                         \
-    }
+template <typename T>                                                        \
+inline void v##name (std::size_t n, const T *a, const T *b, T *y)            \
+{                                                                            \
+    using ns::name;                                                          \
+    for (std::size_t i = 0; i != n; ++i)                                     \
+        y[i] = name(a[i], b[i]);                                             \
+}
 
 #define VSMC_DEFINE_VMATH_B(name, op) \
-    template <typename T>                                                    \
-    inline void v##name (std::size_t n, const T *a, const T *b, T *y)        \
-    {                                                                        \
-        for (std::size_t i = 0; i != n; ++i)                                 \
-            y[i] = a[i] op b[i];                                             \
-    }
+template <typename T>                                                        \
+inline void v##name (std::size_t n, const T *a, const T *b, T *y)            \
+{                                                                            \
+    for (std::size_t i = 0; i != n; ++i)                                     \
+        y[i] = a[i] op b[i];                                                 \
+}
 
 namespace vsmc {
 
@@ -215,45 +215,177 @@ inline void vinvcbrt (std::size_t n, const T *a, T *y)
 
 #include <mkl_vml.h>
 
+#ifndef VSMC_VMATH_VML_THRESHOLD_abs
+#define VSMC_VMATH_VML_THRESHOLD_abs 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_acos
+#define VSMC_VMATH_VML_THRESHOLD_acos 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_acosh
+#define VSMC_VMATH_VML_THRESHOLD_acosh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_asin
+#define VSMC_VMATH_VML_THRESHOLD_asin 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_asinh
+#define VSMC_VMATH_VML_THRESHOLD_asinh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_atan
+#define VSMC_VMATH_VML_THRESHOLD_atan 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_atanh
+#define VSMC_VMATH_VML_THRESHOLD_atanh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_cbrt
+#define VSMC_VMATH_VML_THRESHOLD_cbrt 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_cos
+#define VSMC_VMATH_VML_THRESHOLD_cos 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_cosh
+#define VSMC_VMATH_VML_THRESHOLD_cosh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_erf
+#define VSMC_VMATH_VML_THRESHOLD_erf 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_erfc
+#define VSMC_VMATH_VML_THRESHOLD_erfc 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_exp
+#define VSMC_VMATH_VML_THRESHOLD_exp 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_expm1
+#define VSMC_VMATH_VML_THRESHOLD_expm1 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_inv
+#define VSMC_VMATH_VML_THRESHOLD_inv 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_invcbrt
+#define VSMC_VMATH_VML_THRESHOLD_invcbrt 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_invsqrt
+#define VSMC_VMATH_VML_THRESHOLD_invsqrt 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_lgamma
+#define VSMC_VMATH_VML_THRESHOLD_lgamma 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_log
+#define VSMC_VMATH_VML_THRESHOLD_log 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_log10
+#define VSMC_VMATH_VML_THRESHOLD_log10 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_log1p
+#define VSMC_VMATH_VML_THRESHOLD_log1p 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_sin
+#define VSMC_VMATH_VML_THRESHOLD_sin 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_sinh
+#define VSMC_VMATH_VML_THRESHOLD_sinh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_sqr
+#define VSMC_VMATH_VML_THRESHOLD_sqr 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_sqrt
+#define VSMC_VMATH_VML_THRESHOLD_sqrt 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_tan
+#define VSMC_VMATH_VML_THRESHOLD_tan 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_tanh
+#define VSMC_VMATH_VML_THRESHOLD_tanh 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_tgamma
+#define VSMC_VMATH_VML_THRESHOLD_tgamma 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_add
+#define VSMC_VMATH_VML_THRESHOLD_add 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_div
+#define VSMC_VMATH_VML_THRESHOLD_div 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_hypot
+#define VSMC_VMATH_VML_THRESHOLD_hypot 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_mul
+#define VSMC_VMATH_VML_THRESHOLD_mul 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_pow
+#define VSMC_VMATH_VML_THRESHOLD_pow 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_sub
+#define VSMC_VMATH_VML_THRESHOLD_sub 1000
+#endif
+
+#ifndef VSMC_VMATH_VML_THRESHOLD_powx
+#define VSMC_VMATH_VML_THRESHOLD_powx 1000
+#endif
+
 #define VSMC_DEFINE_VMATH_VML_1(name, vname) \
-    inline void v##name                                                      \
-    (std::size_t n, const float *a, float *y)                                \
-    {::vs##vname(static_cast<MKL_INT>(n), a, y);}                            \
-    inline void v##name                                                      \
-    (std::size_t n, float *a, float *y)                                      \
-    {::vs##vname(static_cast<MKL_INT>(n), a, y);}                            \
-    inline void v##name                                                      \
-    (std::size_t n, const double *a, double *y)                              \
-    {::vd##vname(static_cast<MKL_INT>(n), a, y);}                            \
-    inline void v##name                                                      \
-    (std::size_t n, double *a, double *y)                                    \
-    {::vd##vname(static_cast<MKL_INT>(n), a, y);}
+inline void v##name                                                          \
+(std::size_t n, const float *a, float *y)                                    \
+{                                                                            \
+    n < VSMC_VMATH_VML_THRESHOLD_##name ?                                    \
+        v##name<float>(n, a, y):                                             \
+        ::vs##vname(static_cast<MKL_INT>(n), a, y);                          \
+}                                                                            \
+inline void v##name                                                          \
+(std::size_t n, const double *a, double *y)                                  \
+{                                                                            \
+    n < VSMC_VMATH_VML_THRESHOLD_##name ?                                    \
+        v##name<double>(n, a, y):                                            \
+        ::vd##vname(static_cast<MKL_INT>(n), a, y);                          \
+}
 
 #define VSMC_DEFINE_VMATH_VML_2(name, vname) \
-    inline void v##name                                                      \
-    (std::size_t n, const float *a, const float *b, float *y)                \
-    {::vs##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, float *a, const float *b, float *y)                      \
-    {::vs##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, const float *a, float *b, float *y)                      \
-    {::vs##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, float *a, float *b, float *y)                            \
-    {::vs##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, const double *a, const double *b, double *y)             \
-    {::vd##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, double *a, const double *b, double *y)                   \
-    {::vd##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, const double *a, double *b, double *y)                   \
-    {::vd##vname(static_cast<MKL_INT>(n), a, b, y);}                         \
-    inline void v##name                                                      \
-    (std::size_t n, double *a, double *b, double *y)                         \
-    {::vd##vname(static_cast<MKL_INT>(n), a, b, y);}
+inline void v##name                                                          \
+(std::size_t n, const float *a, const float *b, float *y)                    \
+{                                                                            \
+    n < VSMC_VMATH_VML_THRESHOLD_##name ?                                    \
+        v##name<float>(n, a, b, y):                                          \
+        ::vs##vname(static_cast<MKL_INT>(n), a, b, y);                       \
+}                                                                            \
+inline void v##name                                                          \
+(std::size_t n, const double *a, const double *b, double *y)                 \
+{                                                                            \
+    n < VSMC_VMATH_VML_THRESHOLD_##name ?                                    \
+        v##name<double>(n, a, b, y):                                         \
+        ::vd##vname(static_cast<MKL_INT>(n), a, b, y);                       \
+}
 
 namespace vsmc {
 
@@ -296,16 +428,18 @@ VSMC_DEFINE_VMATH_VML_2(pow,   Pow)
 VSMC_DEFINE_VMATH_VML_2(sub,   Sub)
 
 inline void vpowx (std::size_t n, const float *a, float b, float *y)
-{::vsPowx(static_cast<MKL_INT>(n), a, b, y);}
-
-inline void vpowx (std::size_t n, float *a, float b, float *y)
-{::vsPowx(static_cast<MKL_INT>(n), a, b, y);}
+{
+    n < VSMC_VMATH_VML_THRESHOLD_powx ?
+        vpowx<float>(n, a, b, y):
+        ::vsPowx(static_cast<MKL_INT>(n), a, b, y);
+}
 
 inline void vpowx (std::size_t n, const double *a, double b, double *y)
-{::vdPowx(static_cast<MKL_INT>(n), a, b, y);}
-
-inline void vpowx (std::size_t n, double *a, double b, double *y)
-{::vdPowx(static_cast<MKL_INT>(n), a, b, y);}
+{
+    n < VSMC_VMATH_VML_THRESHOLD_powx ?
+        vpowx<double>(n, a, b, y):
+        ::vdPowx(static_cast<MKL_INT>(n), a, b, y);
+}
 
 } // namespace vsmc::math
 
