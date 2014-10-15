c2cbb9f9ee569495ef560835565411cbf6c9335e
Yan Zhou <zhouyan@me.com>
2014-10-15 14:53:54 +0800

    Partial Revert "remove find constexp engine cmake test"
    This reverts commit 3647a1c637c4efce5566b5ce66158444a9c591a4.

---
+++ cmake/vSMCFindConstexprEngine.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// cmake/vSMCFindConstexprEngine.cpp
+//----------------------------------------------------------------------------
+//
+//                         vSMC: Scalable Monte Carlo
+//
+// This file is distribured under the 2-clauses BSD License.
+// See LICENSE for details.
+//============================================================================
+
+#include <iostream>
+#include <vsmc/cxx11/random.hpp>
+#include <stdint.h>
+
+#define NO_EXPANSION
+
+template<uint64_t, uint64_t>
+struct IntegerRangeTypeTrait {typedef int type;};
+
+template<>
+struct IntegerRangeTypeTrait<0, ~((uint32_t)0)> {typedef uint32_t type;};
+
+template<>
+struct IntegerRangeTypeTrait<0, ~((uint64_t)0)> {typedef uint64_t type;};
+
+int main ()
+{
+    std::cout << sizeof(IntegerRangeTypeTrait<
+            vsmc::cxx11::mt19937::min NO_EXPANSION (),
+            vsmc::cxx11::mt19937::max NO_EXPANSION ()>::type) << std::endl;
+
+    return 0;
+}
