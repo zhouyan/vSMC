2b3ba68a6e5d7d1c7843ed7cb884bedeb5c33bb9
Yan Zhou <zhouyan@me.com>
2014-10-18 01:37:58 +0800

    use VSMC_DEFINE* macro name consistent with the project

--- include/vsmc/rng/u01.hpp
+++ include/vsmc/rng/u01.hpp
@@ -14,7 +14,7 @@
 #include <vsmc/rng/internal/common.hpp>
 #include <vsmc/rng/u01.h>
 
-#define VSMC_DEFINE_U01(FPType, Left, Right, left, right, UBits, FBits) \
+#define VSMC_DEFINE_RNG_U01(FPType, Left, Right, left, right, UBits, FBits) \
 template <> struct U01<Left, Right, uint##UBits##_t, FPType>                 \
 {                                                                            \
     FPType operator() (uint##UBits##_t u) const                              \
@@ -38,67 +38,67 @@ template <typename, typename, typename, typename> struct U01;
 
 /// \brief Converting 32-bits unsigned to single precision uniform \f$[0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Closed, Closed, closed, closed, 32, 24)
+VSMC_DEFINE_RNG_U01(float, Closed, Closed, closed, closed, 32, 24)
 
 /// \brief Converting 32-bits unsigned to single precision uniform \f$[0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Closed, Open, closed, open, 32, 24)
+VSMC_DEFINE_RNG_U01(float, Closed, Open, closed, open, 32, 24)
 
 /// \brief Converting 32-bits unsigned to single precision uniform \f$(0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Open, Closed, open, closed, 32, 24)
+VSMC_DEFINE_RNG_U01(float, Open, Closed, open, closed, 32, 24)
 
 /// \brief Converting 32-bits unsigned to single precision uniform \f$(0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Open, Open, open, open, 32, 24)
+VSMC_DEFINE_RNG_U01(float, Open, Open, open, open, 32, 24)
 
 /// \brief Converting 32-bits unsigned to double precision uniform \f$[0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Closed, Closed, closed, closed, 32, 53)
+VSMC_DEFINE_RNG_U01(double, Closed, Closed, closed, closed, 32, 53)
 
 /// \brief Converting 32-bits unsigned to double precision uniform \f$[0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Closed, Open, closed, open, 32, 53)
+VSMC_DEFINE_RNG_U01(double, Closed, Open, closed, open, 32, 53)
 
 /// \brief Converting 32-bits unsigned to double precision uniform \f$(0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Open, Closed, open, closed, 32, 53)
+VSMC_DEFINE_RNG_U01(double, Open, Closed, open, closed, 32, 53)
 
 /// \brief Converting 32-bits unsigned to double precision uniform \f$(0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Open, Open, open, open, 32, 53)
+VSMC_DEFINE_RNG_U01(double, Open, Open, open, open, 32, 53)
 
 /// \brief Converting 64-bits unsigned to single precision uniform \f$[0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Closed, Closed, closed, closed, 64, 24)
+VSMC_DEFINE_RNG_U01(float, Closed, Closed, closed, closed, 64, 24)
 
 /// \brief Converting 64-bits unsigned to single precision uniform \f$[0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Closed, Open, closed, open, 64, 24)
+VSMC_DEFINE_RNG_U01(float, Closed, Open, closed, open, 64, 24)
 
 /// \brief Converting 64-bits unsigned to single precision uniform \f$(0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Open, Closed, open, closed, 64, 24)
+VSMC_DEFINE_RNG_U01(float, Open, Closed, open, closed, 64, 24)
 
 /// \brief Converting 64-bits unsigned to single precision uniform \f$(0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(float, Open, Open, open, open, 64, 24)
+VSMC_DEFINE_RNG_U01(float, Open, Open, open, open, 64, 24)
 
 /// \brief Converting 64-bits unsigned to double precision uniform \f$[0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Closed, Closed, closed, closed, 64, 53)
+VSMC_DEFINE_RNG_U01(double, Closed, Closed, closed, closed, 64, 53)
 
 /// \brief Converting 64-bits unsigned to double precision uniform \f$[0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Closed, Open, closed, open, 64, 53)
+VSMC_DEFINE_RNG_U01(double, Closed, Open, closed, open, 64, 53)
 
 /// \brief Converting 64-bits unsigned to double precision uniform \f$(0,1]\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Open, Closed, open, closed, 64, 53)
+VSMC_DEFINE_RNG_U01(double, Open, Closed, open, closed, 64, 53)
 
 /// \brief Converting 64-bits unsigned to double precision uniform \f$(0,1)\f$
 /// \ingroup U01
-VSMC_DEFINE_U01(double, Open, Open, open, open, 64, 53)
+VSMC_DEFINE_RNG_U01(double, Open, Open, open, open, 64, 53)
 
 } // namespace vsmc
 
