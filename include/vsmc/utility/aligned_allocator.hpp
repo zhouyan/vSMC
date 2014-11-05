05965e1bf70c792bcc689e043e79d3fcb75c1940
Yan Zhou <zhouyan@me.com>
2014-11-06 02:50:34 +0800

    move internal functions to vsmc namespace

--- include/vsmc/utility/aligned_allocator.hpp
+++ include/vsmc/utility/aligned_allocator.hpp
@@ -35,36 +35,52 @@
     VSMC_STATIC_ASSERT_UTILITY_ALIGNED_ALLOCATOR_POWER_OF_TWO(Alignment);    \
     VSMC_STATIC_ASSERT_UTILITY_ALIGNED_ALLOCATOR_SIZEOF_VOID(Alignment);
 
-namespace vsmc {
+#define VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR_POWER_OF_TWO(alignment) \
+    VSMC_RUNTIME_ASSERT((alignment != 0 && (alignment & (alignment - 1)) == 0),\
+            "**aligned_malloc** USED WITH ALIGNMENT NOT A POWER OF TWO")
 
-namespace internal {
+#define VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR_SIZEOF_VOID(alignemnt) \
+    VSMC_RUNTIME_ASSERT((alignment >= sizeof(void *)),                        \
+            "**aligned_malloc** USED WITH ALIGNMENT LESS THAN sizeof(void *)")
+
+#define VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR \
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR_POWER_OF_TWO(alignment);    \
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR_SIZEOF_VOID(alignment);
+
+namespace vsmc {
 
 #if VSMC_HAS_POSIX
 
-template <std::size_t Alignment>
-inline void *aligned_malloc (std::size_t n)
+/// \brief Aligned malloc
+/// \ingroup AlignedAllocator
+inline void *aligned_malloc (std::size_t n, std::size_t alignment)
 {
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR;
+
     if (n == 0)
         return VSMC_NULLPTR;
 
     void *ptr;
-    if (posix_memalign(&ptr, Alignment, n) != 0)
+    if (posix_memalign(&ptr, alignment, n) != 0)
         throw std::bad_alloc();
 
     return ptr;
 }
 
+/// \brief Aligned free
+/// \ingroup AlignedAllocator
 inline void aligned_free (void *ptr) {free(ptr);}
 
 #elif defined(_WIN32)
 
-template <std::size_t Alignment>
-inline void *aligned_malloc (std::size_t n)
+inline void *aligned_malloc (std::size_t n, std::size_t alignment)
 {
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR;
+
     if (n == 0)
         return VSMC_NULLPTR;
 
-    void *ptr = _aligned_malloc(n, Alignment);
+    void *ptr = _aligned_malloc(n, alignment);
     if (ptr == VSMC_NULLPTR)
         throw std::bad_alloc();
 
@@ -75,26 +91,32 @@ inline void aligned_free (void *ptr) {_aligned_free(ptr);}
 
 #elif VSMC_HAS_TBB
 
-template <std::size_t Alignment>
-inline void *aligned_malloc (std::size_t n)
+inline void *aligned_malloc (std::size_t n, std::size_t alignment)
 {
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR;
+
     if (n == 0)
         return VSMC_NULLPTR;
 
-    return scalable_aligned_malloc(n, Alignment);
+    void *ptr = scalable_aligned_malloc(n, alignment);
+    if (ptr == VSMC_NULLPTR)
+        throw std::bad_alloc();
+
+    return ptr;
 }
 
 inline void aligned_free (void *ptr) {scalable_aligned_free(ptr);}
 
 #elif VSMC_HAS_MKL
 
-template <std::size_t Alignment>
-inline void *aligned_malloc (std::size_t n)
+inline void *aligned_malloc (std::size_t n, std::size_t alignment)
 {
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR;
+
     if (n == 0)
         return VSMC_NULLPTR;
 
-    void *ptr = mkl_malloc(n, static_cast<int>(Alignment));
+    void *ptr = mkl_malloc(n, static_cast<int>(alignment));
     if (ptr == VSMC_NULLPTR)
         throw std::bad_alloc();
 
@@ -105,18 +127,19 @@ inline void aligned_free (void *ptr) {mkl_free(ptr);}
 
 #else // VSMC_HAS_POSIX
 
-template <std::size_t Alignment>
-inline void *aligned_malloc (std::size_t n)
+inline void *aligned_malloc (std::size_t n, std::size_t alignment)
 {
+    VSMC_RUNTIME_ASSERT_UTILITY_ALIGNED_ALLOCATOR;
+
     if (n == 0)
         return VSMC_NULLPTR;
 
-    void *orig_ptr = std::malloc(n + Alignment + sizeof(void *));
+    void *orig_ptr = std::malloc(n + alignment + sizeof(void *));
     if (orig_ptr == VSMC_NULLPTR)
         throw std::bad_alloc();
 
     uintptr_t address = reinterpret_cast<uintptr_t>(orig_ptr);
-    uintptr_t offset = Alignment - (address + sizeof(void *)) % Alignment;
+    uintptr_t offset = alignment - (address + sizeof(void *)) % alignment;
     void *ptr = reinterpret_cast<void *>(address + offset + sizeof(void *));
     void **orig = reinterpret_cast<void **>(address + offset);
     *orig = orig_ptr;
@@ -131,8 +154,6 @@ inline void aligned_free (void *ptr)
 }
 
 #endif // VSMC_HAS_POSIX
-
-} // namespace vsmc::internal
 
 /// \brief Aligned allocator
 /// \ingroup AlignedAllocator
@@ -165,14 +186,10 @@ class AlignedAllocator : public std::allocator<T>
 
     ~AlignedAllocator () {}
 
-    pointer allocate (size_type n,
-            std::allocator<void>::const_pointer = VSMC_NULLPTR)
-    {
-        return static_cast<pointer>(
-                internal::aligned_malloc<Alignment>(sizeof(T) * n));
-    }
+    pointer allocate (size_type n, const void * = VSMC_NULLPTR)
+    {return static_cast<pointer>(aligned_malloc(sizeof(T) * n, Alignment));}
 
-    void deallocate (pointer ptr, size_type) {internal::aligned_free(ptr);}
+    void deallocate (pointer ptr, size_type) {aligned_free(ptr);}
 }; // class AlignedAllocator
 
 } // namespace vsmc
