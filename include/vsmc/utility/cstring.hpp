a94a25c705ced2bd38ab39ca7ba0b474442bc6bc
Yan Zhou <zhouyan@me.com>
2014-11-11 03:39:44 +0800

    Revert "enable non-temporal for memmove for distance bigger than batch size"
    This reverts commit 90615a24ac3f434fac4e10c0647bb6a99ed70790.

--- include/vsmc/utility/cstring.hpp
+++ include/vsmc/utility/cstring.hpp
@@ -493,7 +493,7 @@ inline void *memmove_##simd (void *dst, const void *src, std::size_t n)      \
         }                                                                    \
                                                                              \
         flag = CStringNonTemporalThreshold::instance().over(n);              \
-        flag &= (srca - dsta) >= align * 8 ? 1 : 0;                          \
+        flag &= CStringNonTemporalThreshold::instance().over(srca - dsta);   \
         flag |= (internal::cstring_is_aligned<align>(srcc) << 1) | 2;        \
         internal::cpy_front_##simd(dstc, srcc, n, flag);                     \
                                                                              \
@@ -520,7 +520,7 @@ inline void *memmove_##simd (void *dst, const void *src, std::size_t n)      \
     }                                                                        \
                                                                              \
     flag = CStringNonTemporalThreshold::instance().over(n);                  \
-    flag &= (dsta - srca) >= align * 8 ? 1 : 0;                              \
+    flag &= CStringNonTemporalThreshold::instance().over(dsta - srca);       \
     flag |= (internal::cstring_is_aligned<align>(srcc) << 1) | 2;            \
     internal::cpy_back_##simd(dstc, srcc, n, flag);                          \
                                                                              \
