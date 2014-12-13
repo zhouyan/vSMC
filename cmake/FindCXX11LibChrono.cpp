dbc6033b00bd429bd8d7b7bcea2b35083a071ad1
Yan Zhou <zhouyan@me.com>
2014-12-13 20:09:42 +0800

    test chrono for msvc but assert the duration

--- cmake/FindCXX11LibChrono.cpp
+++ cmake/FindCXX11LibChrono.cpp
@@ -38,9 +38,8 @@ int main ()
         std::chrono::system_clock::now();
     std::chrono::time_point<std::chrono::system_clock> et =
         std::chrono::system_clock::now();
-    std::cout <<
-        std::chrono::duration_cast<std::chrono::microseconds>(et - bt)
-        .count() << std::endl;
+    assert(std::chrono::duration_cast<std::chrono::nanoseconds>(et - bt) <
+            1000000);
 
     return 0;
 }
