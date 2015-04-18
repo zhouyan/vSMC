7e77704f6f1197938b024660009a39f10d12c0e7
Yan Zhou <zhouyan@me.com>
2015-04-18 17:59:40 +0800

    no longer save traces

--- example/pf/include/pf_mpi_do.hpp
+++ example/pf/include/pf_mpi_do.hpp
@@ -48,34 +48,16 @@ inline void cv_do (vsmc::ResampleScheme res, char **argv,
                 cv_state<Order>, BASE_MONITOR>(cv_est<Order>));
     sampler.monitor("pos").name(0) = "pos.x";
     sampler.monitor("pos").name(1) = "pos.y";
+    sampler.initialize(argv[1]);
+    sampler.iterate(DataNum - 1);
+
     std::stringstream ss;
     ss << name << ".r" << sampler.particle().value().world().rank();
     std::string rname(ss.str());
-
-#if VSMC_HAS_HDF5
-    sampler.initialize(argv[1]);
-    vsmc::hdf5store(sampler.particle(), argv[2] + rname + ".trace.h5",
-            "Trace.0");
-    for (std::size_t i = 0; i != DataNum - 1; ++i) {
-        std::stringstream tss;
-        tss << "Trace." << (i + 1);
-        sampler.iterate();
-        vsmc::hdf5store(sampler.particle(), argv[2] + rname + ".trace.h5",
-                tss.str(), true);
-    }
-#else
-    sampler.initialize(argv[1]);
-    sampler.iterate(DataNum - 1);
-#endif
-
     std::string est_file_name(argv[2] + rname + ".tsv");
     std::ofstream est_file;
     est_file.open(est_file_name.c_str());
     est_file << sampler << std::endl;
-    est_file.close();
-    est_file_name = argv[2] + rname + ".trace.tsv";
-    est_file.open(est_file_name.c_str());
-    est_file << sampler.particle().value() << std::endl;
     est_file.close();
 
 #if VSMC_HAS_HDF5
