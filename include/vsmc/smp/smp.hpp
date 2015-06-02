1ef3891ff782b99b2bbd89c010b1484922b5bf40
Yan Zhou <zhouyan@me.com>
2015-06-02 13:33:43 +0800

    Alias OMP backend to SEQ when OpenMP not available

--- include/vsmc/smp/smp.hpp
+++ include/vsmc/smp/smp.hpp
@@ -36,9 +36,7 @@
 
 #include <vsmc/smp/backend_base.hpp>
 #include <vsmc/smp/backend_seq.hpp>
-#if VSMC_HAS_OMP
 #include <vsmc/smp/backend_omp.hpp>
-#endif
 #if VSMC_HAS_TBB
 #include <vsmc/smp/backend_tbb.hpp>
 #endif
