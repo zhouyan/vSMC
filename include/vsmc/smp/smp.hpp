1020000178c7bdc5d78aa946462eda6b709b7971
Yan Zhou <zhouyan@me.com>
2015-06-02 13:35:06 +0800

    sort headers

--- include/vsmc/smp/smp.hpp
+++ include/vsmc/smp/smp.hpp
@@ -35,8 +35,8 @@
 #include <vsmc/internal/config.hpp>
 
 #include <vsmc/smp/backend_base.hpp>
-#include <vsmc/smp/backend_seq.hpp>
 #include <vsmc/smp/backend_omp.hpp>
+#include <vsmc/smp/backend_seq.hpp>
 #if VSMC_HAS_TBB
 #include <vsmc/smp/backend_tbb.hpp>
 #endif
