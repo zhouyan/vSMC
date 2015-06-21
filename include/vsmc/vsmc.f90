b4bd4cc267cda065c0ac5a37d21d93bf56e70449
Yan Zhou <zhouyan@me.com>
2015-06-21 16:30:01 +0800

    reformat

--- include/vsmc/vsmc.f90
+++ include/vsmc/vsmc.f90
@@ -1,5 +1,5 @@
 ! ============================================================================
-!  vSMC/include/vsmc/vsmc.h
+!  vSMC/include/vsmc/vsmc.hpp
 ! ----------------------------------------------------------------------------
 !                          vSMC: Scalable Monte Carlo
 ! ----------------------------------------------------------------------------
@@ -12,7 +12,7 @@
 !    Redistributions of source code must retain the above copyright notice,
 !    this list of conditions and the following disclaimer.
 !
-!    Redistributions in binary form must replicationroduce the above copyright notice,
+!    Redistributions in binary form must reproduce the above copyright notice,
 !    this list of conditions and the following disclaimer in the documentation
 !    and/or other materials provided with the distribution.
 !
@@ -22,9 +22,9 @@
 !  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 !  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 !  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-!  SUBSTITUTE GOODS OR SERVICES LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 !  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-!  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) bind(c)
+!  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 !  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 !  POSSIBILITY OF SUCH DAMAGE.
 ! ============================================================================
