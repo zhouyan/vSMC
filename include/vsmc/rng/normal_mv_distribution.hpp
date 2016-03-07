97c4ac471c6bffdfb0eb38916e53a54f09899d6a
Yan Zhou <zhouyan@me.com>
2016-03-07 10:23:40 +0800

    use enable_if instead of static assert

--- include/vsmc/rng/normal_mv_distribution.hpp
+++ include/vsmc/rng/normal_mv_distribution.hpp
@@ -36,14 +36,6 @@
 #include <vsmc/rng/normal_distribution.hpp>
 #include <vsmc/utility/covariance.hpp>
 
-#define VSMC_STATIC_ASSERT_RNG_NORMAL_MV_DISTRIBUTION_FIXED_DIM(Dim)          \
-    VSMC_STATIC_ASSERT((Dim != Dynamic),                                      \
-        "**NormalMVDistribution** OBJECT DECLARED WITH DYNAMIC DIMENSION")
-
-#define VSMC_STATIC_ASSERT_RNG_NORMAL_MV_DISTRIBUTION_DYNAMIC_DIM(Dim)        \
-    VSMC_STATIC_ASSERT((Dim == Dynamic),                                      \
-        "**NormalMVDistribution** OBJECT DECLARED WITH FIXED DIMENSION")
-
 namespace vsmc
 {
 
@@ -77,17 +69,17 @@ class NormalMVDistribution
         using distribution_type = NormalMVDistribution<RealType, Dim>;
 
         explicit param_type(const result_type *mean = nullptr,
-            const result_type *chol = nullptr)
+            const result_type *chol = nullptr,
+            typename std::enable_if<Dim != Dynamic, void>::type * = nullptr)
         {
-            VSMC_STATIC_ASSERT_RNG_NORMAL_MV_DISTRIBUTION_FIXED_DIM(Dim);
             init(mean, chol);
         }
 
         explicit param_type(std::size_t dim, const result_type *mean = nullptr,
-            const result_type *chol = nullptr)
+            const result_type *chol = nullptr,
+            typename std::enable_if<Dim == Dynamic, void>::type * = nullptr)
             : mean_(dim), chol_(dim * (dim + 1) / 2)
         {
-            VSMC_STATIC_ASSERT_RNG_NORMAL_MV_DISTRIBUTION_DYNAMIC_DIM(Dim);
             init(mean, chol);
         }
 
@@ -176,7 +168,7 @@ class NormalMVDistribution
         }
     }; // class param_type
 
-    /// \brief Only usable when `Dim > 0`
+    /// \brief Only usable when `Dim != Dynamic`
     ///
     /// \param mean Mean vector, if it is a null pointer, then the mean is a
     /// zero vector
