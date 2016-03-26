a8bbc46bb96044a15486d6de275f7c578c1617ec
Yan Zhou <zhouyan@me.com>
2016-03-26 17:56:59 +0800

    index for varying sample size

--- include/vsmc/resample/index.hpp
+++ include/vsmc/resample/index.hpp
@@ -34,10 +34,14 @@
 
 #include <vsmc/internal/common.hpp>
 
+#define VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter_back, func)        \
+    VSMC_RUNTIME_ASSERT((iter <= iter_back && iter_back < iter_size()),       \
+        "**ResampleIndex::" #func "** ITERATION NUMBERS OUT OF RANGE")
+
 namespace vsmc
 {
 
-/// \brief Record and trace resample index
+/// \brief Record and trace resampling index
 /// \ingroup Resample
 template <typename IntType = std::size_t>
 class ResampleIndex
@@ -49,10 +53,20 @@ class ResampleIndex
     std::size_t iter_size() const { return iter_size_; }
 
     /// \brief The sample size of the last iteration
-    std::size_t size() const { return index_.back().size(); }
+    std::size_t size() const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(0, 0, size);
+
+        return index_.back().size();
+    }
 
     /// \brief The sample size of a given iteration
-    std::size_t size(std::size_t iter) const { return index_[iter].size(); }
+    std::size_t size(std::size_t iter) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter, size);
+
+        return index_[iter].size();
+    }
 
     /// \brief Reset history
     void reset() { iter_size_ = 0; }
@@ -60,7 +74,7 @@ class ResampleIndex
     /// \brief Release memory
     void clear() { index_.clear(); }
 
-    /// \brief Push back history of one iteration without resampling
+    /// \brief Append an identity resampling index
     void push_back(std::size_t N)
     {
         ++iter_size_;
@@ -71,47 +85,190 @@ class ResampleIndex
             index_[iter_size_ - 1] = identity_;
     }
 
-    /// \brief Push back history of one iteration with resampling index
+    /// \brief Append a resampling index
     template <typename InputIter>
     void push_back(std::size_t N, InputIter first)
     {
-        push_back(N);
+        ++iter_size_;
+        if (index_.size() < iter_size_)
+            index_.push_back(vsmc::Vector<index_type>(N));
         std::copy_n(first, N, index_[iter_size_ - 1].begin());
     }
 
-    /// \brief Insert at the back of the history without resampling
+    /// \brief Insert at last iteration an identity resampling index
     void insert(std::size_t N)
     {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(0, 0, insert);
+
         resize_identity(N);
         std::copy_n(identity_.begin(), N, index_[iter_size_ - 1].begin());
     }
 
-    /// \brief Insert at the back of the history with resampling index
+    /// \brief Insert at last iteration an identity resampling index
     template <typename InputIter>
     void insert(std::size_t N, InputIter first)
     {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(0, 0, insert);
+
         std::copy_n(first, N, index_[iter_size_ - 1].begin());
     }
 
-    /// \brief Insert at a given iteration with resampling index
+    /// \brief Insert at a given iteration a resampling index
     template <typename InputIter>
     void insert(std::size_t N, std::size_t iter, InputIter first)
     {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter, insert);
+
         std::copy_n(first, N, index_[iter].begin());
     }
 
-    /// \brief Get the index given the particle ID and iteration number,
-    /// starting with zero.
-    index_type index(size_type id, std::size_t iter) const
+    index_type index(std::size_t id) const
     {
-        std::size_t iter_current = iter_size_ - 1;
+        return index(id, iter_size_ - 1, 0);
+    }
+
+    index_type index(std::size_t id, std::size_t iter_back) const
+    {
+        return index(id, iter_back, 0);
+    }
+
+    /// \brief Get the index given the particle ID and iteration number
+    index_type index(
+        std::size_t id, std::size_t iter_back, std::size_t iter) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter_back, insert);
+
         index_type idx = index_.back()[id];
-        while (iter_current != iter) {
-            --iter_current;
-            idx = index_[iter_current][idx];
+        while (iter_back > iter) {
+            --iter_back;
+            idx = index_[iter_back][idx];
         }
 
         return idx;
+    }
+
+    std::size_t index_matrix_nrow() const
+    {
+        return index_matrix_nrow(iter_size_ - 1);
+    }
+
+    std::size_t index_matrix_nrow(std::size_t iter_back) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter_back, iter_back, insert);
+
+        return index_[iter_back].size();
+    }
+
+    std::size_t index_matrix_ncol() const
+    {
+        return index_matrix_ncol(0, iter_size_ - 1);
+    }
+
+    std::size_t index_matrix_ncol(std::size_t iter_back) const
+    {
+        return index_matrix_ncol(iter_back, 0);
+    }
+
+    std::size_t index_matrix_ncol(
+        std::size_t iter_back, std::size_t iter) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter_back, insert);
+
+        return iter_back - iter + 1;
+    }
+
+    Vector<index_type> index_matrix(MatrixLayout layout) const
+    {
+        return index_matrix(layout, iter_size_ - 1, 0);
+    }
+
+    Vector<index_type> index_matrix(
+        MatrixLayout layout, std::size_t iter_back) const
+    {
+        return index_matrix(layout, iter_back, 0);
+    }
+
+    /// \brief Get the resampling index matrix.
+    Vector<index_type> index_matrix(
+        MatrixLayout layout, std::size_t iter_back, std::size_t iter) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter_back, insert);
+
+        Vector<index_type> idxmat(
+            index_matrix_nrow(iter_back) * index_matrix_ncol(iter_back, iter));
+        read_index_matrix(layout, idxmat.begin(), iter_back, iter);
+
+        return idxmat;
+    }
+
+    template <typename RandomIter>
+    RandomIter read_index_matrix(MatrixLayout layout, RandomIter first) const
+    {
+        return read_index_matrix(layout, first, iter_size_ - 1, 0);
+    }
+
+    template <typename RandomIter>
+    RandomIter read_index_matrix(
+        MatrixLayout layout, RandomIter first, std::size_t iter_back) const
+    {
+        return read_index_matrix(layout, first, iter_back, 0);
+    }
+
+    /// \brief Read the resampling index matrix into an random access iterator
+    ///
+    /// \details
+    /// The matrix, say \f$M\f$ is assume to be \f$N\f$ by \f$R\f$, where
+    /// \f$R\f$ is the number of iterations between `iter` and `iter_back`,
+    /// inclusive; and \f$N\f$ is the sample size at iteration `iter_back`. The
+    /// output is equivalent to set the \f$M_{i,j}\f$ to
+    /// `index(i, j, iter_back)`. Note that, column major will be more
+    /// efficient.
+    template <typename RandomIter>
+    RandomIter read_index_matrix(MatrixLayout layout, RandomIter first,
+        std::size_t iter_back, std::size_t iter) const
+    {
+        VSMC_RUNTIME_ASSERT_RESAMPLE_INDEX_ITER(iter, iter_back, insert);
+
+        using difference_type =
+            typename std::iterator_traits<RandomIter>::difference_type;
+        const std::size_t N = index_matrix_nrow(iter_back);
+        const std::size_t R = index_matrix_ncol(iter_back, iter);
+
+        if (layout == RowMajor) {
+            RandomIter back = first + static_cast<difference_type>(R - 1);
+            const index_type *idx = index_[iter_back].data();
+            for (std::size_t i = 0; i != N; ++i)
+                back[static_cast<difference_type>(i * R)] = idx[i];
+            for (std::size_t j = iter_back - 1; j >= iter; --j) {
+                RandomIter last = back;
+                --back;
+                idx = index_[j].data();
+                for (std::size_t i = 0; i != N; ++i) {
+                    back[static_cast<difference_type>(i * R)] =
+                        idx[static_cast<std::size_t>(
+                            last[static_cast<difference_type>(i * R)])];
+                }
+            }
+        }
+
+        if (layout == ColMajor) {
+            RandomIter back =
+                first + static_cast<difference_type>(N * (R - 1));
+            const index_type *idx = index_[iter_back].data();
+            std::copy_n(idx, N, back);
+            for (std::size_t j = iter_back - 1; j >= iter; --j) {
+                RandomIter last = back;
+                back -= static_cast<difference_type>(N);
+                idx = index_[j].data();
+                for (std::size_t i = 0; i != N; ++i) {
+                    back[static_cast<difference_type>(i)] =
+                        idx[static_cast<std::size_t>(
+                            last[static_cast<difference_type>(i)])];
+                }
+            }
+        }
+
+        return first + static_cast<difference_type>(N * R);
     }
 
     private:
@@ -119,7 +276,7 @@ class ResampleIndex
     Vector<index_type> identity_;
     Vector<Vector<index_type>> index_;
 
-    resize_identity(std::size_t N)
+    void resize_identity(std::size_t N)
     {
         std::size_t n = identity_.size();
         identity_.resize(N);
