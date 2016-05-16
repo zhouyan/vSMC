1183397f923fd50328e54cd18d7539b1dd84b9ca
Yan Zhou <zhouyan@me.com>
2016-05-16 21:18:56 +0800

    remove progress and program option

--- manual/prg/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/Documents/GitHub/Script/OS/`uname`
-
-make -j$ncpu -f Makefile.`uname` $1
--- manual/prg/program_option.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./program_option --vec 1 2 1e-1 --str "abc" --vec 8 9 --str "def hij" --n 2 4
